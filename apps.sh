0034d231ae5e86b93cc293c564d546ef8439f79e
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2018-02-23 12:00:32 +0100

    Automate Chrome setup

--- apps.sh
+++ apps.sh
@@ -1,5 +1,11 @@
 pushd
 
+# Chrome
+cd ~/AUR
+git clone https://aur.archlinux.org/google-chrome.git
+cd google-chrome.git
+makepkg -si --noconfirm
+
 # Git
 sudo pacman -S git
 cp `dirname $0`/home/greg/.gitconfig ~
