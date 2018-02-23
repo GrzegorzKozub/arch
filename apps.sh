27e2fb0a3c3b45ebf7c0eca3c8a8a5972cc45f7a
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2018-02-23 12:10:47 +0100

    Automate zsh setup

--- apps.sh
+++ apps.sh
@@ -1,5 +1,9 @@
 pushd
 
+# zsh
+sudo pacman -S --noconfirm zsh
+cp `dirname $0`/home/greg/.zshrc ~
+
 # Chrome
 cd ~/AUR
 git clone https://aur.archlinux.org/google-chrome.git
@@ -7,7 +11,7 @@ cd google-chrome.git
 makepkg -si --noconfirm
 
 # Git
-sudo pacman -S git
+sudo pacman -S --noconfirm git
 cp `dirname $0`/home/greg/.gitconfig ~
 
 popd
