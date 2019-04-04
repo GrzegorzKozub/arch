179952da27cc33997a71d19a8775e201f6ea13d5
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2019-04-04 18:27:47 +0200

    Script most gnome terminal settings

--- config.sh
+++ config.sh
@@ -31,3 +31,13 @@ gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
 
+UUID=$(gsettings get org.gnome.Terminal.ProfilesList default)
+UUID=${UUID:1:-1}
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/default-size-columns" 100
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/default-size-rows" 25
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/font" "'Fira Code Medium 12'"
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/audible-bell" false
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/use-theme-colors" false
+dconf write "/org/gnome/terminal/legacy/profiles:/:$UUID/palette" "['rgb(7,54,66)', 'rgb(220,50,47)', 'rgb(133,153,0)', 'rgb(181,137,0)', 'rgb(38,139,210)', 'rgb(211,54,130)', 'rgb(42,161,152)', 'rgb(238,232,213)', 'rgb(0,43,54)', 'rgb(203,75,22)', 'rgb(88,110,117)', 'rgb(101,123,131)', 'rgb(131,148,150)', 'rgb(108,113,196)', 'rgb(147,161,161)', 'rgb(253,246,227)']"
+unset UUID
+
