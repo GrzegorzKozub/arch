37957c8d6a3cee0cea1f49faf327eb0c04474157
greg <grzegorz.kozub@gmail.com>
2019-12-22 13:16:02 +0100

    Install gnome-vfs to be able to browse smb shares from nautilus

--- system.sh
+++ system.sh
@@ -27,7 +27,7 @@ pacstrap /mnt \
   intel-ucode \
   efibootmgr \
   sudo reflector dialog wpa_supplicant zsh \
-  xf86-video-intel xorg-server gnome gnome-tweak-tool networkmanager \
+  xf86-video-intel xorg-server gnome gnome-tweak-tool networkmanager gnome-vfs \
   git
 
 # fstab file
