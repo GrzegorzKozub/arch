52005f09657e65b413faa411feef36a4e47a57d6
greg <grzegorz.kozub@gmail.com>
2020-01-07 18:24:45 +0100

    Install ecs-cli

--- aws.sh
+++ aws.sh
@@ -2,7 +2,11 @@ set -e -o verbose
 
 # aws
 
+sudo curl -o /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
+
 if [ ! -d ~/.aws ]; then mkdir ~/.aws; fi
+if [ ! -d ~/.ecs ]; then mkdir ~/.ecs; fi
 
 cp `dirname $0`/home/greg/.aws/config ~/.aws
 
