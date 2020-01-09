f1ada5f587476241b7d269697966533d86ee527c
greg <grzegorz.kozub@gmail.com>
2020-01-09 08:32:33 +0100

    Add ranger_devicons

--- ranger.sh
+++ ranger.sh
@@ -8,3 +8,10 @@ if [ ! -d ~/.config/ranger ]; then mkdir ~/.config/ranger; fi
 
 cp `dirname $0`/home/greg/.config/ranger/rc.conf ~/.config/ranger
 
+if [ -d ~/ranger_devicons ]; then rm -rf ~/ranger_devicons; fi
+pushd ~
+git clone https://github.com/alexanderjeurissen/ranger_devicons
+cd ranger_devicons
+make install
+popd
+rm -rf ~/ranger_devicons
