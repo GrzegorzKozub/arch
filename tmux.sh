f933673b683b4bad52d59c30208592d62572b29e
greg <grzegorz.kozub@gmail.com>
2020-01-22 23:10:16 +0100

    Add tpm and tmux-battery to tmux

--- tmux.sh
+++ tmux.sh
@@ -4,5 +4,9 @@ set -e -o verbose
 
 sudo pacman -S --noconfirm tmux
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cp `dirname $0`/home/greg/.tmux.conf ~
+
+~/.tmux/plugins/tpm/bindings/install_plugins
 
