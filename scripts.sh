da63d51871b675827c588975a49901ccdb7271f1
greg <grzegorz.kozub@gmail.com>
2020-01-25 12:44:19 +0100

    Clone themes by default

--- scripts.sh
+++ scripts.sh
@@ -9,3 +9,5 @@ if [ ! -d ~/code ]; then mkdir ~/code; fi
 if [ -d ~/code/arch ]; then rm -rf ~/code/arch; fi
 git clone git@github.com:GrzegorzKozub/arch.git ~/code/arch
 
+if [ -d ~/code/themes ]; then rm -rf ~/code/themes; fi
+git clone git@github.com:GrzegorzKozub/themes.git ~/code/themes
