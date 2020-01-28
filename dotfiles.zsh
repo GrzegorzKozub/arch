118e2f49b3ae17aebd25e360c5b7d7fa2eec0eb9
greg <grzegorz.kozub@gmail.com>
2020-01-28 08:04:49 +0100

    Don't react to some changes

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -15,6 +15,7 @@ pushd ~
 cd ~/code/dotfiles
 git submodule update --init
 git submodule foreach --recursive git checkout master
+git update-index --assume-unchanged .aws/config .config/keepassxc/keepassxc.ini
 popd
 
 cp -rf ~/code/dotfiles/*(DN) ~/
