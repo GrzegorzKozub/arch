37e1efd20c308490f37b6b4b63168cc3bc4263cf
greg <grzegorz.kozub@gmail.com>
2020-02-12 19:51:22 +0100

    Disable awsebcli pip package installation as it breaks pip

--- python.sh
+++ python.sh
@@ -6,12 +6,13 @@ sudo pacman -S --noconfirm python python-pip
 
 pip install --user \
   awscli \
-  awsebcli \
-  aws-shell \
   docker-compose \
   lastversion \
   pynvim
 
-#pip install --user --pre \
-#  vim-vint
+# pip install --user \
+#   awsebcli
+
+pip install --user --pre \
+  vim-vint
 
