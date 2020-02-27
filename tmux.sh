df1be9b9a1af3d38e2c73a5583f51237d2489c45
greg <grzegorz.kozub@gmail.com>
2020-02-27 20:57:20 +0100

    Drop tmux service

--- tmux.sh
+++ tmux.sh
@@ -3,7 +3,3 @@ set -e -o verbose
 # tmux
 
 sudo pacman -S --noconfirm tmux
-
-if [ ! -d ~/.config/systemd/user ]; then mkdir -p ~/.config/systemd/user; fi
-cp `dirname $0`/home/greg/.config/systemd/user/tmux.service ~/.config/systemd/user
-systemctl --user enable tmux
