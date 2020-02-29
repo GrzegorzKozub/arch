b2d9bc7ca0a8d56f5c42a32c3a719d32aee0ed22
greg <grzegorz.kozub@gmail.com>
2020-02-29 09:15:27 +0100

    Fix qt5 window scaling and apperance

--- keepass.sh
+++ keepass.sh
@@ -2,7 +2,10 @@ set -e -o verbose
 
 # keepassxc
 
-sudo pacman -S --noconfirm keepassxc
+sudo pacman -S --noconfirm \
+  keepassxc qt5-styleplugins
 
 cp -r /mnt/.arch/keys/keepass/greg.key ~
 
+if [ ! -d ~/.config/environment.d ]; then mkdir -p ~/.config/environment.d; fi
+cp `dirname $0`/home/greg/.config/environment.d/environment.conf ~/.config/environment.d/
