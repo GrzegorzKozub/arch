23839fbfba2c748c1b331a44aa407a0b8c937c77
greg <grzegorz.kozub@gmail.com>
2020-03-18 08:42:08 +0100

    Make dotfiles init themselves

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -11,12 +11,6 @@ if [ ! -d ~/code ]; then mkdir ~/code; fi
 if [ -d ~/code/dotfiles ]; then rm -rf ~/code/dotfiles; fi
 git clone --recursive git@github.com:GrzegorzKozub/dotfiles.git ~/code/dotfiles
 
-pushd ~
-cd ~/code/dotfiles
-git submodule update --init
-git submodule foreach --recursive git checkout master
-git update-index --assume-unchanged .aws/config .config/keepassxc/keepassxc.ini
-popd
-
 cp -rf ~/code/dotfiles/*(DN) ~/
 rm -rf ~/code/dotfiles
+
