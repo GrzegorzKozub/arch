c758d81be07db9ce1a3b03bc85549bc35101d170
greg <grzegorz.kozub@gmail.com>
2020-03-26 22:15:36 +0100

    Move environment.d to dotfiles

--- keepass.sh
+++ keepass.sh
@@ -5,5 +5,3 @@ set -e -o verbose
 sudo pacman -S --noconfirm \
   keepassxc qt5-styleplugins
 
-if [ ! -d ~/.config/environment.d ]; then mkdir -p ~/.config/environment.d; fi
-cp `dirname $0`/home/greg/.config/environment.d/environment.conf ~/.config/environment.d/
