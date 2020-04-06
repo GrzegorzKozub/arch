d6f474eba7e279144f2adabbe01daa8162255b0e
greg <grzegorz.kozub@gmail.com>
2020-04-06 22:38:05 +0200

    Forget dropox

--- dropbox-fix.sh
+++
@@ -1,7 +0,0 @@
-set -e -o verbose
-
-tar \
-  -xf ~/code/arch/dropbox-fix.tar.gz \
-  -C ~/.dropbox-dist/dropbox-lnx.x86_64-$(cat ~/.dropbox-dist/VERSION)/images/hicolor/16x16/status
-
-dropbox-cli autostart n
