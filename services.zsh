79f64aae4fc405eda46ebdae0070e0267b475ea7
greg <grzegorz.kozub@gmail.com>
2020-05-03 11:32:28 +0200

    Run imwheel on startup on turing

--- services.zsh
+++ services.zsh
@@ -11,8 +11,19 @@ sudo timedatectl set-ntp true
 sudo systemctl enable avahi-daemon.service
 sudo systemctl enable gdm.service
 sudo systemctl enable NetworkManager.service
+
 if [[ $HOST = 'drifter' ]]; then
+
   sudo systemctl enable laptop-mode.service
+
+fi
+
+if [[ $HOST = 'turing' ]]; then
+
+  [[ -d ~/.config/systemd/user ]] || mkdir -p ~/.config/systemd/user
+  cp `dirname $0`/home/greg/.config/systemd/user/imwheel.service ~/.config/systemd/user
+  systemctl --user enable imwheel
+
 fi
 
 # group check
