c71a02a69c6070b9930640ada41a88ddb22a5979
greg <grzegorz.kozub@gmail.com>
2020-05-29 21:40:37 +0200

    Create a wrapper script for qt apps

---
+++ qt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
+# export QT_QPA_PLATFORMTHEME=gtk2
+
+if
+  [[ $HOSTNAME = 'turing' ]] ||
+  [[ $(systemctl --user is-active 4k.service) = 'active' ]]
+then
+  export QT_SCREEN_SCALE_FACTORS=1
+  export QT_SCALE_FACTOR=1
+elif [[ $HOSTNAME = 'drifter' ]]; then
+  export QT_SCREEN_SCALE_FACTORS=2
+  export QT_SCALE_FACTOR=0.9
+fi
+
+$1
