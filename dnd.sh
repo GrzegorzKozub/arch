bfad4873ec057c7e00d0d2060306c441aee1b925
greg <grzegorz.kozub@gmail.com>
2020-06-13 18:02:28 +0200

    Set dnd also on unlock

--- dnd.sh
+++ dnd.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
 gsettings set org.gnome.desktop.notifications show-banners false
+
+gdbus monitor -y -d org.freedesktop.login1 | \
+  grep --line-buffered -i "{'LockedHint': <false>}" | \
+  while read; do
+    gsettings set org.gnome.desktop.notifications show-banners false
+  done
+
