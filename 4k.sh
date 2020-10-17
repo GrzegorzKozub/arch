2955a43cc83355e76e428e78bf6901fba4cb97af
greg <grzegorz.kozub@gmail.com>
2020-10-17 18:13:26 +0200

    Attempt workaround gnome loosing right alt

--- 4k.sh
+++ 4k.sh
@@ -15,3 +15,5 @@ trap on_term SIGTERM
 fonts 1.5
 imwheel -d
 
+# https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/918
+gsettings set org.gnome.desktop.input-sources xkb-options "['lv3:ralt_switch']"
