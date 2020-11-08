040ab78d945a00c925da4f41aef9f140f5c51338
greg <grzegorz.kozub@gmail.com>
2020-11-08 11:23:59 +0100

    Update drifter part number after bitlocker created another one

--- drifter.zsh
+++ drifter.zsh
@@ -5,7 +5,7 @@
 export MY_DISK=/dev/nvme0n1
 export MY_EFI_PART=/dev/nvme0n1p2
 export MY_EFI_PART_NBR=2
-export MY_ARCH_PART=/dev/nvme0n1p7
+export MY_ARCH_PART=/dev/nvme0n1p8
 
 export MY_HOSTNAME=drifter
 
