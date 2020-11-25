584602523ddf1901089f131b238c4d556c31ae4c
greg <grzegorz.kozub@gmail.com>
2020-11-25 20:22:03 +0100

    Support alternate login with yubikey

---
+++ yubikey.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+set -e -o verbose
+
+# yubikey
+
+sudo pacman -Sy --noconfirm \
+  pam-u2f
+
+DIR=${XDG_CONFIG_HOME:-~/.config}/Yubico
+
+[[ -d $DIR ]] || mkdir -p $DIR
+pamu2fcfg -o pam://$HOST -i pam://$HOST > $DIR/u2f_keys
+
+FILE=/etc/pam.d/gdm-password
+
+YUBIKEY="auth       sufficient                  pam_u2f.so origin=pam://$HOST appid=pam://$HOST"
+  LOGIN="auth       include                     system-local-login"
+
+grep -Fxq $YUBIKEY $FILE || sudo sed -i "/^$LOGIN$/i $YUBIKEY" $FILE
+
+unset DIR FILE LOGIN YUBIKEY
+
