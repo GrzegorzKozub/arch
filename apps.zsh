9c308571c4f2ad85f6c64d1bb8d514d26d1eb2bc
greg <grzegorz.kozub@gmail.com>
2020-12-11 18:00:08 +0100

    Install awscli with pacman instead of pip

--- apps.zsh
+++ apps.zsh
@@ -118,9 +118,9 @@ sudo systemctl start docker.service
 
 # aws
 
-sudo curl -o /usr/local/bin/ecs-cli \
-  https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
-sudo chmod +x /usr/local/bin/ecs-cli
+yay -S --aur --noconfirm \
+  aws-cli-v2-bin \
+  aws-ecs-cli
 
 # dev
 
