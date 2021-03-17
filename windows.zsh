10e3429cba109a106e9aeda1a513074fb04dff89
greg <grzegorz.kozub@gmail.com>
2021-03-17 22:18:07 +0100

    Support fixing specific window position

--- windows.zsh
+++ windows.zsh
@@ -1,20 +1,11 @@
 #!/usr/bin/env zsh
 
 function windows {
+
   local res=$(xrandr | grep "*" | sed -n -e "s/^   //p" | sed -n -e "s/     .*$//p")
   local width=$(echo $res | cut -dx -f1)
   local height=$(echo $res | cut -dx -f2)
-
   local theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
-
-  local chrome=".?Chrom(e|ium)$"
-  local vscode=".?Visual Studio Code$"
-  local data_studio=".?Azure Data Studio$"
-  local obs="^OBS.*Profile.*Scenes.?"
-  local shotcut=".?Shotcut$"
-  local slack=".?Slack$"
-  local postman="^Postman$"
-  local keepass=".? - KeePassXC$"
 
   function fix {
     local windows=$(xdotool search --onlyvisible --maxdepth 2 --name --class "$1")
@@ -37,19 +28,6 @@ function windows {
         $(( $width / ( 8 * 2 ) )) \
         $(( ( $height - $top_bar ) / ( 8 * 2 ) + $top_bar + ${3:-0} ))
     }
-
-    center $chrome
-
-    center $vscode
-    center $data_studio
-
-    center $obs 0 $title_bar
-    center $shotcut 0 $title_bar
-
-    center $slack $title_bar
-    center $postman $title_bar
-
-    fix $keepass 1600 1284 800 318
   }
 
   [[ $width = 3840 ]] && [[ $height = 2160 ]] && {
@@ -58,19 +36,8 @@ function windows {
     elif [[ $theme =~ "Materia" ]]; then local top_bar=38; local title_bar=42
     else exit 1; fi
 
-    MARGIN=25
-
-    fix $chrome \
-      $(( ( $width / 5 ) * 3 )) \
-      $(( $height - $MARGIN * 2 - $top_bar )) \
-      $MARGIN \
-      $(( $MARGIN + $top_bar ))
-
-    fix $slack \
-      $(( $width / 2 )) \
-      $(( $height - $MARGIN * 4 - $top_bar - $title_bar )) \
-      $(( $width / 2 - $MARGIN * 2 )) \
-      $(( $MARGIN * 2 + $top_bar ))
+    local _4k=true
+    local margin=25
 
     function center {
       fix "$1" \
@@ -79,22 +46,71 @@ function windows {
         $(( $width / ( 4 * 2 ) )) \
         $(( ( $height - $top_bar ) / ( 8 * 2 ) + $top_bar ))
     }
-
-    center $vscode
-    center $data_studio
-
-    center $obs
-    center $shotcut
-
-    fix $postman \
-      $(( ( $width / 5 ) * 3 )) \
-      $(( ( ( $height - $top_bar ) / 8 ) * 6 - $title_bar )) \
-      $(( $width / 5 )) \
-      $(( ( $height - $top_bar ) / 8 + $top_bar ))
-
-    fix $keepass 1200 964 1320 635
   }
+
+  function chrome {
+    local title=".?Chrom(e|ium)$"
+    [[ -v _4k ]] && {
+      fix $title \
+        $(( ( $width / 5 ) * 3 )) \
+        $(( $height - $margin * 2 - $top_bar )) \
+        $margin \
+        $(( $margin + $top_bar ))
+    } || center $title
+  }
+
+  function data_studio { center ".?Azure Data Studio$" }
+
+  function keepass {
+    local title=".? - KeePassXC$"
+    [[ -v _4k ]] && fix $title 1200 964 1320 635 || fix $title 1600 1284 800 318
+  }
+
+  function obs {
+    local title="^OBS.*Profile.*Scenes.?"
+    [[ -v _4k ]] && center $title || center $title 0 $title_bar
+  }
+
+  function postman {
+    local title="^Postman$"
+    [[ -v _4k ]] && {
+      fix $title \
+        $(( ( $width / 5 ) * 3 )) \
+        $(( ( ( $height - $top_bar ) / 8 ) * 6 - $title_bar )) \
+        $(( $width / 5 )) \
+        $(( ( $height - $top_bar ) / 8 + $top_bar ))
+    } || center $title $title_bar
+  }
+
+  function shotcut {
+    local title=".?Shotcut$"
+    [[ -v _4k ]] && center $title || center $title 0 $title_bar
+  }
+
+  function slack {
+    local title=".?Slack$"
+    [[ -v _4k ]] && {
+      fix $title \
+        $(( $width / 2 )) \
+        $(( $height - $margin * 4 - $top_bar - $title_bar )) \
+        $(( $width / 2 - $margin * 2 )) \
+        $(( $margin * 2 + $top_bar ))
+    } || center $title $title_bar
+  }
+
+  function vscode { center ".?Visual Studio Code$" }
+
+  [[ -z "$1" ]] && {
+    chrome
+    data_studio
+    keepass
+    obs
+    postman
+    shotcut
+    slack
+    vscode
+  } || eval $1
 }
 
-windows
+windows $1
 
