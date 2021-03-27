8f42b281361f37718d8f1aa06f9b22b45a8d11b2
greg <grzegorz.kozub@gmail.com>
2021-03-27 10:06:40 +0100

    Automate backup and restore

--- backup.zsh
+++ backup.zsh
@@ -2,11 +2,16 @@
 
 set -e -o verbose
 
-DIR=/mnt/$(date +%Y%m%d%H%M)
-mkdir $DIR
+() {
 
-fsarchiver savefs $DIR/root.fsa /dev/mapper/vg1-root
-cp /mnt/boot/*.img /mnt/boot/vmlinuz-linux $DIR/
+[[ $(df /dev/mapper/vg1-backup --output=avail | grep -v Avail) -lt 10000000 ]] && {
+  rm -rf "/mnt/$(ls -t /mnt | grep '^[0-9]*$' | tail -n1)"
+}
 
-unset DIR
+local dir=/mnt/$(date +%Y%m%d%H%M)
+sudo mkdir $dir
 
+fsarchiver savefs $dir/root.fsa /dev/mapper/vg1-root
+cp /mnt/boot/*.img /mnt/boot/vmlinuz-linux $dir/
+
+}
--- restore.zsh
+++ restore.zsh
@@ -2,8 +2,13 @@
 
 set -e -o verbose
 
-[[ -d $1 ]] || exit 1
+() {
 
-fsarchiver restfs $1/root.fsa id=0,dest=/dev/mapper/vg1-root
-cp $1/*.img $1/vmlinuz-linux /mnt/boot/
+[[ $1 && -d $1 ]] &&
+  local dir=$1 ||
+  local dir="/mnt/$(ls -t /mnt | grep '^[0-9]*$' | head -n1)"
 
+fsarchiver restfs $dir/root.fsa id=0,dest=/dev/mapper/vg1-root
+cp $dir/*.img $dir/vmlinuz-linux /mnt/boot/
+
+} $1
