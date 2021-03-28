a889d777c36181158e226bb4ba5bb7b4cdf9b785
greg <grzegorz.kozub@gmail.com>
2021-03-28 19:22:30 +0200

    Checks to allow reruns

--- dotfiles.zsh
+++ dotfiles.zsh
@@ -12,7 +12,8 @@ sudo mount /dev/$PENDRIVE /mnt
 
 unset $PENDRIVE
 
-[[ -d ~/.ssh ]] || mkdir ~/.ssh
+[[ -d ~/.ssh || -n ~/.ssh ]] && rm -rf ~/.ssh
+mkdir ~/.ssh
 
 cp /mnt/.arch/keys/openssh/.ssh/config ~/.ssh
 chmod 600 ~/.ssh/config
