5c571fdcb38b7b6af63b5fc2933d16b5aa28631e
greg <grzegorz.kozub@gmail.com>
2021-04-16 10:43:47 +0200

    Archiso will now make 15GB space before backing up

--- backup.zsh
+++ backup.zsh
@@ -9,7 +9,7 @@ set -e
 . $1/unlock.zsh
 . $1/mount.zsh
 
-[[ $(df /dev/mapper/vg1-backup --output=avail | grep -v Avail) -lt 10000000 ]] && {
+[[ $(df /dev/mapper/vg1-backup --output=avail | grep -v Avail) -lt 15000000 ]] && {
   local oldest="/mnt/$(ls -t /mnt | grep '^[0-9]*$' | tail -n1)"
   echo "removing $oldest"
   rm -rf $oldest
