11d6c451c8733293bae3751b3c3146200e60107d
greg <grzegorz.kozub@gmail.com>
2021-08-14 08:15:20 +0200

    Remove baobab and dconf-editor

--- system.zsh
+++ system.zsh
@@ -56,7 +56,7 @@ pacstrap /mnt \
   xorg-server xorg-xrandr \
   gdm gnome-menus gnome-shell gnome-shell-extensions gnome-keyring gvfs gvfs-smb networkmanager xdg-user-dirs-gtk xdg-utils \
   pipewire xdg-desktop-portal \
-  baobab dconf-editor eog evince gnome-calculator gnome-control-center gnome-software gnome-system-monitor gnome-terminal gnome-tweak-tool nautilus
+  eog evince gnome-calculator gnome-control-center gnome-software gnome-system-monitor gnome-terminal gnome-tweak-tool nautilus
 
 # fstab file
 
