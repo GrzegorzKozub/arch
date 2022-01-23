55daf50a3b89099848c0ad7a9743702101fc8a3c
greg <grzegorz.kozub@gmail.com>
2022-01-23 13:32:48 +0100

    Fix old backup removal

--- backup.zsh
+++ backup.zsh
@@ -9,11 +9,11 @@ set -e
 . $1/unlock.zsh
 . $1/mount.zsh
 
-[[ $(df /dev/mapper/vg1-backup --output=avail | grep -v Avail) -lt 12000000 ]] && {
+while [[ $(df /dev/mapper/vg1-backup --output=avail | grep -v Avail) -lt 12000000 ]]; do
   local oldest="/mnt/$(ls -t /mnt | grep '^[0-9]*$' | tail -n1)"
   echo "removing $oldest"
   rm -rf $oldest
-}
+done
 
 local dir=/mnt/$(date +%Y%m%d%H%M)
 echo "backing up to $dir"
