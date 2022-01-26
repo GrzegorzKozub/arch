c42977ac8a16bd759abee1bbc6b05eef254abcbd
greg <grzegorz.kozub@gmail.com>
2022-01-26 08:24:13 +0100

    Find a way to split the qemu params

--- vm.zsh
+++ vm.zsh
@@ -26,11 +26,13 @@ local share=$HOME/Downloads
 
 # dependencies
 
-if [[ ! -f $dir/$windows || ! -f $dir/$virtio ]]; then
-  echo "put windows and virtio-win iso from https://github.com/virtio-win/virtio-win-pkg-scripts in $dir"
-  exit 1
-fi
+#  echo "put windows and virtio-win iso from https://github.com/virtio-win/virtio-win-pkg-scripts in $dir"
 
+# map \\10.0.2.4\qemu
+
+# https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe
+
+# sudo pacman -S virt-viewer
 # disk
 
 if [[ ! -f $dir/$disk ]]; then
@@ -38,36 +40,57 @@ if [[ ! -f $dir/$disk ]]; then
   echo 'use disk (viostor dir), vga and nic drivers from virtio-win and disable fast startup'
 fi
 
-# vm
+# params
 
-# map \\10.0.2.4\qemu
+local opts='-name windows'
 
-# https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe
+opts="$opts -enable-kvm"
 
-# sudo pacman -S virt-viewer
+opts="$opts -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,topoext"
+opts="$opts -smp 4,sockets=1,cores=2,threads=2"
 
-qemu-system-x86_64 \
-  -name windows \
-  -enable-kvm \
-  -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,topoext \
-  -smp 4,sockets=1,cores=2,threads=2 \
-  -m 8G \
-  -vga std \
-  -device ich9-intel-hda \
-  -device hda-output \
-  -nic user,model=virtio-net-pci,smb=$share \
-  -drive file=$dir/$disk,if=virtio,aio=native,cache.direct=on \
-  -drive file=$dir/$windows,media=cdrom \
-  -drive file=$dir/$virtio,media=cdrom \
--device virtio-serial-pci \
--spice port=5930,disable-ticketing=on \
--device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
--chardev spicevmc,id=spicechannel0,name=vdagent \
--device virtserialport,chardev=spicechannel1,name=org.spice-space.webdav.0 \
--chardev spiceport,name=org.spice-space.webdav.0,id=spicechannel1
-  -boot menu=on \
-  -monitor stdio \
--display spice-app
+opts="$opts -m 8G"
 
-}
+opts="$opts -vga std"
+
+opts="$opts -device ich9-intel-hda"
+opts="$opts -device hda-output"
+
+opts="$opts -nic user,model=virtio-net-pci,smb=$share"
+
+opts="$opts -drive file=$dir/$disk,if=virtio,aio=native,cache.direct=on"
+[[ -f $dir/$windows ]] && opts="$opts -drive file=$dir/$windows,media=cdrom"
+[[ -f $dir/$virtio ]] && opts="$opts -drive file=$dir/$virtio,media=cdrom"
+
+
+# todo: better opts
+
+if [[ $1 = 'spice' ]]; then
+
+  opts="$opts -spice port=5930,disable-ticketing=on"
+  opts="$opts -display spice-app"
+  opts="$opts -device virtio-serial-pci"
+
+  # clipboard sharing
+  opts="$opts -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0"
+  opts="$opts -chardev spicevmc,id=spicechannel0,name=vdagent"
+
+  # folder sharing
+  opts="$opts -device virtserialport,chardev=spicechannel1,name=org.spice-space.webdav.0"
+  opts="$opts -chardev spiceport,name=org.spice-space.webdav.0,id=spicechannel1"
+
+fi
+
+opts="$opts -boot menu=on"
+opts="$opts -monitor stdio"
+
+# run
+
+setopt shwordsplit
+
+qemu-system-x86_64 $opts
+
+unsetopt shwordsplit
+
+} $1
 
