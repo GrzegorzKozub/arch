0a9661be6149c43875036057c1a26b3082c79df2
greg <grzegorz.kozub@gmail.com>
2022-02-13 09:22:21 +0100

    Make unlock.zsh idempotent

--- unlock.zsh
+++ unlock.zsh
@@ -6,12 +6,13 @@ set -e
 
 () {
 
-[[ $(lsblk -lno PATH | grep 'vg1') ]] && exit
+[[ $(lsblk -lno PATH | grep 'vg1') ]] || {
 
-local arch_part="$(lsblk -lno PATH,PARTTYPE | grep -i '0FC63DAF-8483-4772-8E79-3D69D8477DE4' | cut -d' ' -f1)"
+  local arch_part="$(lsblk -lno PATH,PARTTYPE | grep -i '0FC63DAF-8483-4772-8E79-3D69D8477DE4' | cut -d' ' -f1)"
 
-cryptsetup luksOpen $arch_part lvm
-sleep 1
+  cryptsetup luksOpen $arch_part lvm
+  sleep 1
+}
 
 }
 
