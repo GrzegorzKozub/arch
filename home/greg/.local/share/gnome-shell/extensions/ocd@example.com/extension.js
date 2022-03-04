c841c3658cf275a9dbd910cfc37cbad6a2433904
greg <grzegorz.kozub@gmail.com>
2022-03-04 16:31:07 +0100

    Solider on with the extension

--- home/greg/.local/share/gnome-shell/extensions/ocd@example.com/extension.js
+++ home/greg/.local/share/gnome-shell/extensions/ocd@example.com/extension.js
@@ -2,79 +2,69 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const Meta = imports.gi.Meta;
 const Shell = imports.gi.Shell;
-const GLib = imports.gi.GLib;
-const Lang = imports.lang;
 
 class Extension {
-  constructor() {}
-  callbackID;
+  windowCreatedHandler;
+  uhd;
 
+  constructor() {}
 
   enable() {
-    const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.ocd');
     Main.wm.addKeybinding(
       'ocd',
-      settings,
+      ExtensionUtils.getSettings('org.gnome.shell.extensions.ocd'),
       Meta.KeyBindingFlags.IGNORE_AUTOREPEAT,
       Shell.ActionMode.NORMAL | Shell.ActionMode.OVERVIEW,
-      this.windows.bind(this));
-
-
-    //let callbackID = global.display.connect('window-created', Lang.bind(this, this._update));
-    this.callbackID = global.display.connect('window-created', this._update);
-  log(this.callbackID);
-  }
-
-  _update(_, mw) {
-    log(`open ${mw.title}`);
+      this.hotkeyPressed.bind(this));
+    this.windowCreatedHandler = global.display.connect(
+      'window-created',
+      this.windowCreated);
+    this.uhd = global.screen_width === 3840 && global.screen_height === 2160;
   }
 
   disable() {
     Main.wm.removeKeybinding('ocd');
-    log(`disco ${this.callbackID}`);
-  global.display.disconnect(this.callbackID);
+    global.display.disconnect(this.windowCreatedHandler);
   }
+
+  hotkeyPressed() {
+    this.windows();
+  }
+
+  windowCreated(_, win) {}
 
   windows() {
     global.get_window_actors().forEach(win => {
-      log(win.meta_window.title);
-      this.center(win.meta_window, 7, 12);
+      this.big(win.meta_window);
     });
-    //log(this.getScreen().laptop);
-
   }
 
-  center(win, w, h) {
-    if (win.get_maximized()) { win.unmaximize(3); }
-    const widthStep = 8, heightStep = 16;
-    const ws = Main.layoutManager.getWorkAreaForMonitor(0)
+  big(win) {
+    if (this.uhd) { this.center(win, 6, 14); } else { this.center(win, 7, 14.5); };
+  }
+
+  medium(win) {
+    if (this.uhd) { this.center(win, 4.5, 12); } else { this.center(win, 6, 12.5); };
+  }
+
+  small(win) {
+    if (this.uhd) { this.center(win, 3, 10); } else { this.center(win, 5, 10.5); };
+  }
+
+  center(win, width, height) {
+    if (win.get_maximized()) {
+      win.unmaximize(Meta.MaximizeFlags.BOTH);
+    }
+    const desktop = Main.layoutManager.getWorkAreaForMonitor(0);
+    const widthStep = 8,
+      heightStep = 16;
     win.move_resize_frame(
       0,
-      ws.x + (ws.width / widthStep * (widthStep - w) / 2),
-      ws.y + (ws.height / heightStep * (heightStep - h) / 2),
-      ws.width / widthStep * w,
-      ws.height / heightStep * h
-    );
-
-    // Meta.MaximizeFlags
-            GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, function() { win.maximize(3); });
+      ((desktop.width / widthStep) * ((widthStep - width) / 2)) + desktop.x,
+      ((desktop.height / heightStep) * ((heightStep - height) / 2)) + desktop.y,
+      (desktop.width / widthStep) * width,
+      (desktop.height / heightStep) * height);
   }
-
-  //getConfig() {
-    //return [
-      //{ app: 'brave', }
-    //];
-  //}
-
-  //getScreen() {
-    //const screen = {};
-    //if (global.screen_width === 3840 && global.screen_height === 2160) {
-      //screen.monitor = true;
-    //} else if (global.screen_width === 3840 && global.screen_height === 2400) {
-      //screen.monitor = false;
-    //}
-    //return screen;
-  //}
 }
 
 function init() {
