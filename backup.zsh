41fe90688f84dfbcbd12077dfed0a4dbfe84e811
greg <grzegorz.kozub@gmail.com>
2022-03-25 07:41:45 +0100

    Use all cpu threads for backup

--- backup.zsh
+++ backup.zsh
@@ -22,7 +22,7 @@ local dir=/mnt/$(date +%Y%m%d%H%M)
 echo "backing up to $dir"
 mkdir $dir
 
-fsarchiver savefs -c - $dir/root.fsa /dev/mapper/vg1-root
+fsarchiver savefs -j $(nproc) -c - $dir/root.fsa /dev/mapper/vg1-root
 cp /mnt/boot/*.img /mnt/boot/vmlinuz-linux* $dir/
 
 umount -R /mnt
