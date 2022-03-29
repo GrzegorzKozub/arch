fdc49e8013f76669853ae0ef334e598bd2303efa
greg <grzegorz.kozub@gmail.com>
2022-03-29 15:25:00 +0200

    Local mysql

--- mysql.zsh
+++ mysql.zsh
@@ -4,6 +4,15 @@ set -e -o verbose
 
 # mysql
 
+docker run \
+  --detach \
+  --env MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+  --name mysql \
+  -p 3306:3306 \
+  mysql:5.7
+
+# mysql-workbench
+
 sudo pacman -S --noconfirm \
   mysql-workbench
 
