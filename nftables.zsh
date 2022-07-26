62e69f1a772eafa25dd01adeb5e518176b5b69ee
greg <grzegorz.kozub@gmail.com>
2022-07-26 16:55:40 +0200

    Must be root to nft

--- nftables.zsh
+++ nftables.zsh
@@ -4,54 +4,54 @@ set -e -o verbose
 
 # reset
 
-nft flush ruleset
+sudo nft flush ruleset
 
 # table
 
-nft add table inet my_table
+sudo nft add table inet my_table
 
 # chains and default policies
 
-nft add chain inet my_table my_input '{ type filter hook input priority 0 ; policy drop ; }'
-nft add chain inet my_table my_forward '{ type filter hook forward priority 0 ; policy drop ; }'
-nft add chain inet my_table my_output '{ type filter hook output priority 0 ; policy accept ; }'
+sudo nft add chain inet my_table my_input '{ type filter hook input priority 0 ; policy drop ; }'
+sudo nft add chain inet my_table my_forward '{ type filter hook forward priority 0 ; policy drop ; }'
+sudo nft add chain inet my_table my_output '{ type filter hook output priority 0 ; policy accept ; }'
 
 # established, related and local connections
 
-nft add rule inet my_table my_input ct state related,established accept
-nft add rule inet my_table my_input iif lo accept
+sudo nft add rule inet my_table my_input ct state related,established accept
+sudo nft add rule inet my_table my_input iif lo accept
 
 # invalid connections
 
-nft add rule inet my_table my_input ct state invalid drop
+sudo nft add rule inet my_table my_input ct state invalid drop
 
 # ping
 
-nft add rule inet my_table my_input meta l4proto ipv6-icmp accept
-nft add rule inet my_table my_input meta l4proto icmp accept
-nft add rule inet my_table my_input ip protocol igmp accept
+sudo nft add rule inet my_table my_input meta l4proto ipv6-icmp accept
+sudo nft add rule inet my_table my_input meta l4proto icmp accept
+sudo nft add rule inet my_table my_input ip protocol igmp accept
 
 # tcp and udp
 
-nft add chain inet my_table my_udp_chain
-nft add chain inet my_table my_tcp_chain
+sudo nft add chain inet my_table my_udp_chain
+sudo nft add chain inet my_table my_tcp_chain
 
-nft add rule inet my_table my_input meta l4proto udp ct state new jump my_udp_chain
-nft add rule inet my_table my_input 'meta l4proto tcp tcp flags & (fin|syn|rst|ack) == syn ct state new jump my_tcp_chain'
+sudo nft add rule inet my_table my_input meta l4proto udp ct state new jump my_udp_chain
+sudo nft add rule inet my_table my_input 'meta l4proto tcp tcp flags & (fin|syn|rst|ack) == syn ct state new jump my_tcp_chain'
 
-nft add rule inet my_table my_input meta l4proto udp reject
-nft add rule inet my_table my_input meta l4proto tcp reject with tcp reset
+sudo nft add rule inet my_table my_input meta l4proto udp reject
+sudo nft add rule inet my_table my_input meta l4proto tcp reject with tcp reset
 
 # ssh
 
-# nft add rule inet my_table my_tcp_chain tcp dport 22 accept
+# sudo nft add rule inet my_table my_tcp_chain tcp dport 22 accept
 
 # http
 
-# nft add rule inet my_table my_tcp_chain tcp dport 80 accept
-# nft add rule inet my_table my_tcp_chain tcp dport 443 accept
+# sudo nft add rule inet my_table my_tcp_chain tcp dport 80 accept
+# sudo nft add rule inet my_table my_tcp_chain tcp dport 443 accept
 
 # other connections
 
-nft add rule inet my_table my_input counter reject with icmpx port-unreachable
+sudo nft add rule inet my_table my_input counter reject with icmpx port-unreachable
 
