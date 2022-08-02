67eb419f10c2c0d81f418572ef7821993329840b
greg <grzegorz.kozub@gmail.com>
2022-08-02 09:06:54 +0200

    No need for local mysql always

--- mysql.zsh
+++ mysql.zsh
@@ -4,12 +4,12 @@ set -e -o verbose
 
 # mysql
 
-docker run \
-  --detach \
-  --env MYSQL_ALLOW_EMPTY_PASSWORD=1 \
-  --name mysql \
-  -p 3306:3306 \
-  mysql:5.7
+# docker run \
+  # --detach \
+  # --env MYSQL_ALLOW_EMPTY_PASSWORD=1 \
+  # --name mysql \
+  # -p 3306:3306 \
+  # mysql:5.7
 
 # mysql-workbench
 
