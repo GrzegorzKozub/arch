ac1cfa569dcd7b18429e27b3e47937e3d7f13ae9
greg <grzegorz.kozub@gmail.com>
2022-08-05 07:57:31 +0200

    Switch from hp-lip to hpuld

--- printer.zsh
+++ printer.zsh
@@ -6,8 +6,13 @@ set -e -o verbose
 
 sudo pacman -Sy --noconfirm \
   cups \
-  hplip \
   system-config-printer
+
+paru -S --aur --noconfirm \
+  hpuld
+
+# sudo pacman -Sy --noconfirm \
+  # hplip hplip-plugin python-gobject python-pyqt5
 
 sudo systemctl enable cups
 sudo systemctl start cups
@@ -16,8 +21,8 @@ sudo systemctl start cups
 
 for APP in \
   cups \
-  hplip \
-  hp-uiscan
+  # hplip \
+  # hp-uiscan
 do
   printf "[Desktop Entry]\nNoDisplay=true" > ~/.local/share/applications/$APP.desktop
 done
