b8b4666af7cff759cac23209d10a2e991e1eba18
greg <grzegorz.kozub@gmail.com>
2022-12-16 08:07:00 +0100

    Use 4 threads for backup compression

--- backup.zsh
+++ backup.zsh
@@ -26,7 +26,7 @@ local dir=$backup/$(date +%Y%m%d%H%M)
 echo "backing up to $dir"
 mkdir $dir
 
-fsarchiver savefs -j $(nproc) -c - $dir/root.fsa /dev/mapper/vg1-root
+fsarchiver savefs -j4 -c - $dir/root.fsa /dev/mapper/vg1-root
 cp /mnt/boot/*.img /mnt/boot/vmlinuz-linux* $dir/
 
 umount -R /mnt
