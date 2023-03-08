ea5704d5691d7cab3cdf55c2817184d9f6a0fe38
greg <grzegorz.kozub@gmail.com>
2023-03-08 22:23:05 +0100

    need to refactor from shrtcuts up

--- plasma.zsh
+++ plasma.zsh
@@ -4,26 +4,61 @@ set -e -o verbose
 
 # https://github.com/shalva97/kde-configuration-files
 
-# locale
+# appearance > global theme
 
-FILE=$XDG_CONFIG_HOME/plasma-localerc
+plasma-apply-lookandfeel --apply 'org.kde.breeze.desktop'
 
-kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_MEASUREMENT' 'pl_PL.UTF-8'
-kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_MONETARY' 'pl_PL.UTF-8'
-kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_NUMERIC' 'pl_PL.UTF-8'
-kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_PAGE' 'pl_PL.UTF-8'
-kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_TIME' 'pl_PL.UTF-8'
+# appearance > plasma style
 
-# keyboard
+kwriteconfig5 --file $XDG_CONFIG_HOME/plasmarc --group 'Theme' --key 'name' 'breeze-dark'
 
-FILE=$XDG_CONFIG_HOME/kxkbrc
+# appearance > window decorations
 
-kwriteconfig5 --file $FILE --group 'Layout' --key 'LayoutList' 'pl'
-kwriteconfig5 --file $FILE --group 'Layout' --key 'Use' 'true'
+FILE=$XDG_CONFIG_HOME/kwinrc
 
-# kwriteconfig5 --file $XDG_CONFIG_HOME/kglobalshortcutsrc --group 'KDE Keyboard Layout Switcher' --key 'Switch to Next Keyboard Layout' 'Meta+Space,none,Switch to Next Keyboard Layout'
+kwriteconfig5 --file $FILE --group 'org.kde.kdecoration2' --key 'ButtonsOnLeft' ''
+kwriteconfig5 --file $FILE --group 'org.kde.kdecoration2' --key 'ButtonsOnRight' 'IAX'
 
-# shortcuts
+# appearance > fonts
+
+# ...
+
+# appearance > icons
+
+/usr/lib/plasma-changeicons 'Papirus'
+
+# appearance > cursors
+
+kwriteconfig5 --file $XDG_CONFIG_HOME/kcminputrc --group 'Mouse' --key 'cursorSize' '36'
+
+# appearance > splash screen
+
+FILE=$XDG_CONFIG_HOME/ksplashrc
+
+kwriteconfig5 --file $FILE --group 'KSplash' --key 'Engine' 'none'
+kwriteconfig5 --file $FILE --group 'KSplash' --key 'Theme' 'None'
+
+# window management > window rules
+
+FILE=$XDG_CONFIG_HOME/kwinrulesrc
+
+[[ $(grep 'Description=kitty' $FILE) ]] || {
+
+  ID=$(uuidgen)
+
+  kwriteconfig5 --file $FILE --group $ID --key 'Description' 'kitty'
+  kwriteconfig5 --file $FILE --group $ID --key 'clientmachine' 'localhost'
+  kwriteconfig5 --file $FILE --group $ID --key 'decocolor' 'BreezeDark'
+  kwriteconfig5 --file $FILE --group $ID --key 'decocolorrule' '2'
+  kwriteconfig5 --file $FILE --group $ID --key 'wmclass' 'kitty'
+  kwriteconfig5 --file $FILE --group $ID --key 'wmclassmatch' '1'
+
+  kwriteconfig5 --file $FILE --group 'General' --key 'count' '1'
+  kwriteconfig5 --file $FILE --group 'General' --key 'rules' $ID
+
+}
+
+# shortcuts > custom shortcuts
 
 shortcut() {
   local file=$XDG_CONFIG_HOME/khotkeysrc
@@ -59,62 +94,44 @@ else
   shortcut 'Print' 'flameshot' 'flameshot gui'
 fi
 
-
 sed -i 's/^Activate Window Demanding Attention=Meta+Ctrl+A/Activate Window Demanding Attention=none/' $XDG_CONFIG_HOME/kglobalshortcutsrc
 
 shortcut 'Meta+Ctrl+A' 'audio output' 'audio.zsh sink'
 shortcut 'Meta+Ctrl+M' 'audio input' 'audio.zsh source'
 shortcut 'Meta+Ctrl+N' 'night light' 'pkill -USR1 redshift'
 
-# mouse and touchpad
+# notifications
 
-FILE=$XDG_CONFIG_HOME/kcminputrc
+kwriteconfig5 --file $XDG_CONFIG_HOME/plasmanotifyrc --group 'DoNotDisturb' --key 'Until' "$[$(date --iso-8601 | cut -d- -f1) + 1],1,1,0,0,0"
 
-[[ $XDG_SESSION_TYPE = 'wayland' ]] && kwriteconfig5 --file $FILE --group 'Mouse' --key 'XLbInptPointerAcceleration' -- '-0.8'
-[[ $XDG_SESSION_TYPE = 'x11' ]] && kwriteconfig5 --file $FILE --group 'Mouse' --key 'XLbInptPointerAcceleration' -- '-0.4'
+# regional settings > region & language
 
-# screen
+FILE=$XDG_CONFIG_HOME/plasma-localerc
+
+kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_MEASUREMENT' 'pl_PL.UTF-8'
+kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_MONETARY' 'pl_PL.UTF-8'
+kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_NUMERIC' 'pl_PL.UTF-8'
+kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_PAGE' 'pl_PL.UTF-8'
+kwriteconfig5 --file $FILE --group 'Formats' --key 'LC_TIME' 'pl_PL.UTF-8'
+
+# input devices > keyboard
+
+FILE=$XDG_CONFIG_HOME/kxkbrc
+
+kwriteconfig5 --file $FILE --group 'Layout' --key 'LayoutList' 'pl'
+kwriteconfig5 --file $FILE --group 'Layout' --key 'Use' 'true'
+
+# input devices > mouse
+
+[[ $XDG_SESSION_TYPE = 'wayland' ]] && VAL='-0.8'
+[[ $XDG_SESSION_TYPE = 'x11' ]] && VAL='-0.4'
+
+kwriteconfig5 --file $XDG_CONFIG_HOME/kcminputrc --group 'Mouse' --key 'XLbInptPointerAcceleration' -- $VAL
+
+# display and monitor
 
 # kscreen-doctor output.DP-4.scale.1.5
 # kwriteconfig5 --file $XDG_CONFIG_HOME/kdeglobals --group 'KScreen' --key 'ScaleFactor' '1.5'
-
-# themes
-
-plasma-apply-lookandfeel --apply 'org.kde.breeze.desktop'
-
-kwriteconfig5 --file $XDG_CONFIG_HOME/plasmarc --group 'Theme' --key 'name' 'breeze-dark'
-
-FILE=$XDG_CONFIG_HOME/kwinrc
-
-kwriteconfig5 --file $FILE --group 'org.kde.kdecoration2' --key 'ButtonsOnLeft' ''
-kwriteconfig5 --file $FILE --group 'org.kde.kdecoration2' --key 'ButtonsOnRight' 'IAX'
-
-FILE=$XDG_CONFIG_HOME/kwinrulesrc
-
-[[ $(grep 'Description=kitty' $FILE) ]] || {
-
-  ID=$(uuidgen)
-
-  kwriteconfig5 --file $FILE --group $ID --key 'Description' 'kitty'
-  kwriteconfig5 --file $FILE --group $ID --key 'clientmachine' 'localhost'
-  kwriteconfig5 --file $FILE --group $ID --key 'decocolor' 'BreezeDark'
-  kwriteconfig5 --file $FILE --group $ID --key 'decocolorrule' '2'
-  kwriteconfig5 --file $FILE --group $ID --key 'wmclass' 'kitty'
-  kwriteconfig5 --file $FILE --group $ID --key 'wmclassmatch' '1'
-
-  kwriteconfig5 --file $FILE --group 'General' --key 'count' '1'
-  kwriteconfig5 --file $FILE --group 'General' --key 'rules' $ID
-
-}
-
-/usr/lib/plasma-changeicons 'Papirus'
-
-kwriteconfig5 --file $XDG_CONFIG_HOME/kcminputrc --group 'Mouse' --key 'cursorSize' '36'
-
-FILE=$XDG_CONFIG_HOME/ksplashrc
-
-kwriteconfig5 --file $FILE --group 'KSplash' --key 'Engine' 'none'
-kwriteconfig5 --file $FILE --group 'KSplash' --key 'Theme' 'None'
 
 # wallpapers
 
@@ -131,40 +148,52 @@ sed -i 's/^thickness=.*$/thickness=80/' $XDG_CONFIG_HOME/plasmashellrc
 
 FILE=$XDG_CONFIG_HOME/plasma-org.kde.plasma.desktop-appletsrc
 
-sed -i '/^plugin=org.kde.plasma.pager$/d' $FILE
-sed -i '/^plugin=org.kde.plasma.showdesktop$/d' $FILE
+sed -i -f - $FILE << END
+  /^plugin=org.kde.plasma.pager$/d
+  /^plugin=org.kde.plasma.showdesktop$/d
+END
 
-echo "$(grep 'org.kde.plasma.kickoff' $FILE --before-context=2 | grep 'Containments')[Configuration][General]" >> $FILE
-echo 'alphaSort=true' >> $FILE
-echo 'applicationsDisplay=0' >> $FILE
-echo 'primaryActions=3' >> $FILE
-echo 'showActionButtonCaptions=false' >> $FILE
-echo 'systemFavorites=lock-screen\\,logout\\,save-session\\,switch-user\\,suspend\\,hibernate\\,reboot\\,shutdown' >> $FILE
+# panel > application launcher
 
-echo "$(grep 'org.kde.plasma.icontasks' $FILE --before-context=2 | grep 'Containments')[Configuration][General]" >> $FILE
-# echo 'iconSpacing=3' >> $FILE
-echo 'indicateAudioStreams=false' >> $FILE
-echo 'launchers=applications:org.kde.dolphin.desktop,applications:kitty.desktop,applications:code.desktop,applications:postman.desktop,preferred://browser,applications:org.keepassxc.KeePassXC.desktop' >> $FILE
+cat << END >> $FILE
+$(grep --before-context=2 'org.kde.plasma.kickoff' $FILE | grep 'Containments')[Configuration][General]
+alphaSort=true
+applicationsDisplay=0
+primaryActions=3
+showActionButtonCaptions=false
+systemFavorites=lock-screen\\,logout\\,save-session\\,switch-user\\,suspend\\,hibernate\\,reboot\\,shutdown
+END
 
-sed -i '/^plugin=org.kde.plasma.mediacontroller$/d' $FILE
-sed -i '/^plugin=org.kde.kscreen$/d' $FILE
-sed -i -E 's/^(extraItems=.*)(,org.kde.plasma.mediacontroller)(.*)/\1\3/' $FILE
-sed -i -E 's/^(extraItems=.*)(,org.kde.kscreen)(.*)/\1\3/' $FILE
-sed -i '/^hiddenItems=.*$/d' $FILE
-sed -i '/^extraItems=.*$/a hiddenItems=org.kde.plasma.clipboard,org.kde.plasma.keyboardlayout,org.kde.plasma.notifications' $FILE
-sed -i '/^shownItems=.*$/d' $FILE
+# panel > icons-only task manager
 
-echo "$(grep 'org.kde.plasma.digitalclock' $FILE --before-context=2 | grep 'Containments')[Configuration][Appearance]" >> $FILE
-echo 'customDateFormat=d MMM' >> $FILE
-echo 'dateFormat=custom' >> $FILE
-echo "$(grep 'org.kde.plasma.digitalclock' $FILE --before-context=2 | grep 'Containments')[Configuration][Apperance]" >> $FILE
-echo 'showDate=false' >> $FILE
+cat << END >> $FILE
+$(grep --before-context=2 'org.kde.plasma.icontasks' $FILE | grep 'Containments')[Configuration][General]
+indicateAudioStreams=false
+launchers=applications:org.kde.dolphin.desktop,applications:kitty.desktop,applications:code.desktop,applications:postman.desktop,applications:brave-browser.desktop,applications:org.keepassxc.KeePassXC.desktop
+END
 
-# dnd
+# panel > system tray
 
-kwriteconfig5 --file $XDG_CONFIG_HOME/plasmanotifyrc --group 'DoNotDisturb' --key 'Until' "$[$(date --iso-8601 | cut -d- -f1) + 1],1,1,0,0,0"
+sed -i -r -f - $FILE << END
+  /^plugin=org.kde.kscreen$/d
+  /^plugin=org.kde.plasma.mediacontroller$/d
+  s/^(extraItems=.*)(,org.kde.kscreen)(.*)/\1\3/
+  s/^(extraItems=.*)(,org.kde.plasma.mediacontroller)(.*)/\1\3/
+  /^hiddenItems=.*$/d
+  /^extraItems=.*$/a hiddenItems=org.kde.plasma.clipboard,org.kde.plasma.keyboardlayout,org.kde.plasma.notifications
+  /^shownItems=.*$/d
+END
 
-# restart apps for some changes to take effect
+# panel > digital clock
 
-# kquitapp5 plasmashell && kstart5 plasmashell &
+cat << END >> $FILE
+$(grep --before-context=2 'org.kde.plasma.digitalclock' $FILE | grep 'Containments')[Configuration][Appearance]
+customDateFormat=d MMM
+dateFormat=custom
+END
+
+cat << END >> $FILE
+$(grep --before-context=2 'org.kde.plasma.digitalclock' $FILE | grep 'Containments')[Configuration][Apperance]
+showDate=false
+END
 
