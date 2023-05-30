401cb5b86dc1e716f648e284e9b443764fe9cf49
greg <grzegorz.kozub@gmail.com>
2023-05-30 07:10:58 +0200

    skip links for foot

--- links.zsh
+++ links.zsh
@@ -68,21 +68,21 @@ fi
 
 # foot
 
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.foot \
-  org.codeberg.dnkl.footclient
-do
-  cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
-  sed -i '2iStartupWMClass=foot' $XDG_DATA_HOME/applications/$APP.desktop
-done
+# for APP in \
+#   org.codeberg.dnkl.foot-server \
+#   org.codeberg.dnkl.foot \
+#   org.codeberg.dnkl.footclient
+# do
+#   cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
+#   sed -i '2iStartupWMClass=foot' $XDG_DATA_HOME/applications/$APP.desktop
+# done
 
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.footclient
-do
-  sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
-done
+# for APP in \
+#   org.codeberg.dnkl.foot-server \
+#   org.codeberg.dnkl.footclient
+# do
+#   sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
+# done
 
 # kitty
 
