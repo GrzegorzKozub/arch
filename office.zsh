6082982f5c883041f7efaff9e26b6fcac2ade9f9
greg <grzegorz.kozub@gmail.com>
2023-06-02 08:57:58 +0200

    power point

--- office.zsh
+++ office.zsh
@@ -13,6 +13,7 @@ sudo pacman -S --noconfirm \
 sudo sed -i -e 's/Logo=1/Logo=0/' /etc/libreoffice/sofficerc
 
 xdg-mime default libreoffice-calc.desktop application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+xdg-mime default libreoffice-impress.desktop application/vnd.openxmlformats-officedocument.presentationml.presentation
 xdg-mime default libreoffice-writer.desktop application/vnd.openxmlformats-officedocument.wordprocessingml.document
 
 # links
