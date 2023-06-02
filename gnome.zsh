f0ec7e0fa1bdd819f95955cbf40232855d784375
greg <grzegorz.kozub@gmail.com>
2023-06-02 08:26:46 +0200

    restore flameshot shortcut

--- gnome.zsh
+++ gnome.zsh
@@ -208,6 +208,7 @@ add_shortcut() {
   CUSTOM_KEYBINDINGS+="'$dir'"
 }
 
+add_shortcut 0 'Print' 'flameshot' 'flameshot gui'
 add_shortcut 1 '<Control><Super>a' 'audio output' "/home/$USER/code/arch/audio.zsh sink"
 add_shortcut 2 '<Control><Super>m' 'audio input' "/home/$USER/code/arch/audio.zsh source"
 
