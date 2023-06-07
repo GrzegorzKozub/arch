73bafa1c73ae1a33ffce37a8c6b15d5709f1bcd9
greg <grzegorz.kozub@gmail.com>
2023-06-07 07:31:46 +0200

    color profiles on worker via colormgr

--- settings.zsh
+++ settings.zsh
@@ -2,6 +2,36 @@
 
 set -e -o verbose
 
+# color
+
+if [[ $HOST = 'worker' ]]; then
+
+  add_color_profile() {
+    set +e
+    PROFILE=$(colormgr find-profile-by-filename $1.icm |
+      grep 'Profile ID' | sed -e 's/Profile ID:    //')
+    if [[ -z $PROFILE ]]; then
+      (exit 1)
+      while [[ ! $? = 0 ]]; do
+        echo 'import'
+        PROFILE=$(
+          colormgr import-profile `dirname $0`/home/$USER/.local/share/icc/$1.icm |
+            grep 'Profile ID' | sed -e 's/Profile ID:    //')
+      done
+    fi
+    echo $PROFILE
+    DEVICE=$(colormgr find-device-by-property Model $2 |
+      grep 'Device ID' | sed -e 's/Device ID:     //')
+    echo $DEVICE
+    colormgr device-add-profile $DEVICE $PROFILE
+    set -e
+  }
+
+  add_color_profile '27ul850-w' 'LG HDR 4K'
+  add_color_profile '27ud88-w' 'LG Ultra HD'
+
+fi
+
 # sound
 
 pactl list short sinks | cut -f1 | while read -r id; do
