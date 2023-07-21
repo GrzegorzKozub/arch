89c23e83c178cc5bdbb006869684400daa0ea889
greg <grzegorz.kozub@gmail.com>
2023-07-21 08:26:44 +0200

    foot and kitty font size 13 works on drifter where display scale is at 200%

--- hyprland.zsh
+++ hyprland.zsh
@@ -10,26 +10,7 @@ sudo pacman -S --noconfirm \
   hyprpaper \
   waybar wofi \
   gammastep \
-  grim slurp \
-  foot
-
-# foot
-
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.foot \
-  org.codeberg.dnkl.footclient
-do
-  cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
-  sed -i '2iStartupWMClass=foot' $XDG_DATA_HOME/applications/$APP.desktop
-done
-
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.footclient
-do
-  sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
-done
+  grim slurp
 
 # dotfiles
 
--- sway.zsh
+++ sway.zsh
@@ -14,7 +14,6 @@ sudo pacman -S --noconfirm \
   gammastep \
   pavucontrol \
   grim slurp \
-  foot \
   swayimg
 
 paru -S --aur --noconfirm \
@@ -30,24 +29,6 @@ for APP in \
   swayimg
 do
   cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
-  sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
-done
-
-# foot
-
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.foot \
-  org.codeberg.dnkl.footclient
-do
-  cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
-  sed -i '2iStartupWMClass=foot' $XDG_DATA_HOME/applications/$APP.desktop
-done
-
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.footclient
-do
   sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
 done
 
