fddc6f1b3d5c4f5c9c718ddd3c1108867eefe8a4
greg <grzegorz.kozub@gmail.com>
2023-08-09 15:33:54 +0200

    hide gammastep in gnome when hyprland is installed

--- hyprland.zsh
+++ hyprland.zsh
@@ -18,6 +18,16 @@ paru -S --aur --noconfirm \
   ttf-material-design-icons-extended \
   chayang-git
 
+# links
+
+for APP in \
+  gammastep \
+  gammastep-indicator
+do
+  cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
+  sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
+done
+
 # dotfiles
 
 . ~/code/dotfiles/hyprland.zsh
