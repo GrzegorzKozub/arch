5840952f36c180767c81faa9aea4adc659620730
greg <grzegorz.kozub@gmail.com>
2023-08-13 06:49:20 +0200

    rem sway

--- hyprland.zsh
+++ hyprland.zsh
@@ -4,25 +4,31 @@ set -e -o verbose
 
 # packages
 
+paru -S --aur --noconfirm \
+  hyprland-git
+
 sudo pacman -S --noconfirm \
   xdg-desktop-portal-hyprland \
-  hyprland \
   hyprpaper \
   swayidle swaylock \
   waybar wofi \
   gammastep \
   brightnessctl \
+  pavucontrol \
   grim slurp
 
 paru -S --aur --noconfirm \
   ttf-material-design-icons-extended \
   chayang-git
 
+# polkit swayimg
+
 # links
 
 for APP in \
   gammastep \
-  gammastep-indicator
+  gammastep-indicator \
+  pavucontrol
 do
   cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
   sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
