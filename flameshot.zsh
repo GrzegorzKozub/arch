d0c15da543993401d390987bd8789ba4ae491a36
greg <grzegorz.kozub@gmail.com>
2023-10-16 09:01:50 +0200

    ocd

--- flameshot.zsh
+++ flameshot.zsh
@@ -2,17 +2,9 @@
 
 set -e
 
-flameshot gui
-
-# When flameshot gui is bound directly to a keyboard skortcut then
-# every time the Print Screen kee is pressed we get:
-#
-# xdg-desktop-por[4606]: Failed to show access dialog:
-# GDBus.Error:org.freedesktop.DBus.Error.AccessDenied:
-# Only the focused app is allowed to show a system access dialog
-#
-# Binding this script fixes this issue.
-#
+# work around "Only the focused app is allowed to show a system access dialog" in journal
 # https://github.com/flameshot-org/flameshot/issues/2868
 # https://github.com/flameshot-org/flameshot/issues/3213
+
+flameshot gui
 
