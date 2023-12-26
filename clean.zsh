8b90a9837724a12181ce909df24988bc50b71c68
greg <grzegorz.kozub@gmail.com>
2023-12-26 11:56:43 +0100

    clean update

--- clean.zsh
+++ clean.zsh
@@ -4,13 +4,15 @@ set -e -o verbose
 
 # remove obsolete dirs and files
 
+[[ -d ~/.gnome ]] && rm -rf ~/.gnome
+# [[ -f ~/.npm ]] && rm -rf ~/.npm
+
+# [[ -f ~/.yarnrc ]] && rm -f ~/.yarnrc
 [[ -f ~/.zshrc ]] && rm -f ~/.zshrc
 
-[[ -d ~/.gnome ]] && rm -rf ~/.gnome
-
 # [[ -d ~/.cache/js-v8flags ]] && rm -rf ~/.cache/js-v8flags
+# [[ -d ~/.cache/node ]] && rm -rf ~/.cache/node
 # [[ -d ~/.cache/yarn ]] && rm -rf ~/.cache/yarn
-# [[ -f ~/.yarnrc ]] && rm ~/.yarnrc
 
 (exit 0)
 
