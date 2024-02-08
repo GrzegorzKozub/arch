b2f5f6ed42f3b5ff4a647ef1f003963f7e74256c
greg <grzegorz.kozub@gmail.com>
2024-02-08 15:21:08 +0100

    have to clean cargo dir before installing rust

--- migrate.zsh
+++ migrate.zsh
@@ -13,5 +13,8 @@ sudo pacman -Rs --noconfirm \
 sudo pacman -Rs --noconfirm \
   rust
 
+[[ -d $XDG_DATA_HOME/cargo ]] && rm -rf $XDG_DATA_HOME/cargo
+[[ -d $XDG_DATA_HOME/rustup ]] && rm -rf $XDG_DATA_HOME/rustup
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 
