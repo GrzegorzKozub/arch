8b6a721409d19eb1ba3329c9ae0d8214c553bce6
greg <grzegorz.kozub@gmail.com>
2024-02-09 20:46:46 +0100

    clean migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,24 +2,3 @@
 
 set -o verbose
 
-# elixir, ruby
-
-sudo pacman -Rs --noconfirm \
-  elixir \
-  ruby
-
-# rust
-
-sudo pacman -Rs --noconfirm \
-  rust
-
-[[ -d $XDG_DATA_HOME/cargo ]] && rm -rf $XDG_DATA_HOME/cargo
-[[ -d $XDG_DATA_HOME/rustup ]] && rm -rf $XDG_DATA_HOME/rustup
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
-
-# debugedit
-
-sudo pacman -S --noconfirm \
-  base-devel
-
