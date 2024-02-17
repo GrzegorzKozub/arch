46690078ad60797a83d09f48ab8c714c32fb6c0e
greg <grzegorz.kozub@gmail.com>
2024-02-17 08:13:07 +0100

    new drifter part

--- drifter.zsh
+++ drifter.zsh
@@ -5,7 +5,7 @@
 export MY_DISK=/dev/nvme0n1
 export MY_EFI_PART=/dev/nvme0n1p1
 export MY_EFI_PART_NBR=1
-export MY_ARCH_PART=/dev/nvme0n1p7
+export MY_ARCH_PART=/dev/nvme0n1p8
 
 export MY_HOSTNAME=drifter
 
