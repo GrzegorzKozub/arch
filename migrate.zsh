8f73fb75b6b610f8b07894f5d09b2e9aa55ee3ba
greg <grzegorz.kozub@gmail.com>
2024-03-19 08:56:51 +0100

    reinstall paru

--- migrate.zsh
+++ migrate.zsh
@@ -1,14 +1,36 @@
 #!/usr/bin/env zsh
 
-# set -o verbose
-#
-# sudo pacman -S --noconfirm \
-#   go-yq \
-#   silicon \
-#   zoxide
-#
-# paru -S --aur --noconfirm \
-#   ttf-victor-mono
+set -o verbose
+
+# paru
+
+[[ -d ~/paru ]] && rm -rf ~/paru
+
+pushd ~
+
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm
+
+popd
+
+rm -rf ~/paru
+rm -rf ~/.cargo
+
+sudo pacman -Rs --noconfirm \
+  rust
+
+# apps
+
+sudo pacman -S --noconfirm \
+  go-yq \
+  silicon \
+  zoxide
+
+paru -S --aur --noconfirm \
+  ttf-victor-mono
+
+# fetch
 
 cp `dirname $0`/home/$USER/.config/systemd/user/fetch.service $XDG_CONFIG_HOME/systemd/user
 systemctl --user enable fetch.service
