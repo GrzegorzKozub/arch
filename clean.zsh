e3198c725b3aaeb4338a66d0c8179c5720c00de0
greg <grzegorz.kozub@gmail.com>
2024-03-26 07:59:28 +0100

    obsidian now fixed

--- clean.zsh
+++ clean.zsh
@@ -5,15 +5,17 @@ set -e -o verbose
 # remove obsolete dirs and files
 
 [[ -d ~/.gnome ]] && rm -rf ~/.gnome
-[[ -d ~/.npm ]] && rm -rf ~/.npm
-[[ -d ~/.yarn ]] && rm -rf ~/.yarn
 
-[[ -f ~/.yarnrc ]] && rm -f ~/.yarnrc
 [[ -f ~/.zshrc ]] && rm -f ~/.zshrc
 
-[[ -d ~/.cache/js-v8flags ]] && rm -rf ~/.cache/js-v8flags
 [[ -d ~/.cache/node ]] && rm -rf ~/.cache/node
+[[ -d ~/.npm ]] && rm -rf ~/.npm
+
 [[ -d ~/.cache/yarn ]] && rm -rf ~/.cache/yarn
+[[ -d ~/.yarn ]] && rm -rf ~/.yarn
+[[ -f ~/.yarnrc ]] && rm -f ~/.yarnrc
+
+[[ -d ~/.cache/js-v8flags ]] && rm -rf ~/.cache/js-v8flags
 
 (exit 0)
 
--- migrate.zsh
+++ migrate.zsh
@@ -2,6 +2,3 @@
 
 set -o verbose
 
-# https://aur.archlinux.org/packages/obsidian-bin
-sudo pacman -U --noconfirm /run/media/greg/data/obsidian-bin-1.5.11-1-x86_64.pkg.tar
-
