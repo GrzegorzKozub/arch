a36d7971a50f89300c30292e3885255b5f036700
greg <grzegorz.kozub@gmail.com>
2024-04-24 09:26:19 +0200

    file type draw.io

--- draw.zsh
+++ draw.zsh
@@ -15,3 +15,7 @@ sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/drawio.desktop
 cp /usr/share/applications/drawio.desktop $XDG_DATA_HOME/applications/draw.io.desktop
 sed -i -e 's/^Name=.*/Name=draw.io/' $XDG_DATA_HOME/applications/draw.io.desktop
 
+# file types
+
+xdg-mime default draw.io.desktop application/vnd.jgraph.mxfile
+
