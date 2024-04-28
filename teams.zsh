1a7c9b9a7f6b736a9c9d7dca349c8b57987f060d
greg <grzegorz.kozub@gmail.com>
2024-04-28 06:46:24 +0200

    teams fav

--- teams.zsh
+++ teams.zsh
@@ -12,6 +12,14 @@ paru -S --aur --noconfirm \
 cp /usr/share/applications/teams-for-linux.desktop $XDG_DATA_HOME/applications
 sed -i -e 's/^Name=.*/Name=Teams/' $XDG_DATA_HOME/applications/teams-for-linux.desktop
 
+[[ $XDG_CURRENT_DESKTOP = 'GNOME' ]] && {
+  FAVS=$(gsettings get org.gnome.shell favorite-apps)
+  [[ $(echo $FAVS | grep 'teams-for-linux.desktop') ]] || {
+    FAVS=$(echo $FAVS | sed "s/\]/, 'teams-for-linux.desktop']/")
+    gsettings set org.gnome.shell favorite-apps $FAVS
+  }
+}
+
 # cleanup
 
 . `dirname $0`/packages.zsh
