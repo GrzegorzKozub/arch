a58b3dcfe91e5b9dcc382f33bd6a789f688229c0
greg <grzegorz.kozub@gmail.com>
2024-05-07 07:42:18 +0200

    migrate cleanup

--- migrate.zsh
+++ migrate.zsh
@@ -4,25 +4,7 @@ set -o verbose
 
 # https://aur.archlinux.org/packages/aws-cli-v2
 
-sudo sed -ie \
-  's/#IgnorePkg   =/IgnorePkg    = aws-cli-v2/' \
-  /etc/pacman.conf
-
-# neovim
-
-paru -S --aur --noconfirm \
-  fswatch
-
-# vpn
-
-nmcli connection delete apsis
-
-sudo pacman -Rs --noconfirm \
-  openconnect networkmanager-openconnect
-
-. ~/code/keys/init.zsh
-
-# fontconfig
-
-cp `dirname $0`/home/$USER/.config/fontconfig/fonts.conf $XDG_CONFIG_HOME/fontconfig
+# sudo sed -ie \
+#   's/#IgnorePkg   =/IgnorePkg    = aws-cli-v2/' \
+#   /etc/pacman.conf
 
