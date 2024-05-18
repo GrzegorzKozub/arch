36c8835dd5236bf4c17e87498a8840c5ef9f7926
greg <grzegorz.kozub@gmail.com>
2024-05-18 06:35:42 +0200

    reset migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,23 +2,4 @@
 
 set -o verbose
 
-# https://aur.archlinux.org/packages/aws-cli-v2
-
-sudo sed -ie \
-  's/IgnorePkg    = aws-cli-v2/#IgnorePkg   =/' \
-  /etc/pacman.conf
-
-sudo pacman -Rs --noconfirm \
-  aws-cli-v2
-
-paru -S --aur --noconfirm --nocheck \
-  aws-cli-v2
-
-# https://github.com/flexagoon/rounded-window-corners
-
-paru -Rs --noconfirm \
-  gnome-shell-extension-rounded-window-corners-git
-
-paru -S --aur --noconfirm \
-  gnome-shell-extension-rounded-window-corners-reborn
 
