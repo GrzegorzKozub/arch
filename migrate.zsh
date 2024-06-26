f57412e80ee834e7417bc41a38d71b7871a0b84d
greg <grzegorz.kozub@gmail.com>
2024-06-26 21:15:15 +0200

    set hibernation file size

--- migrate.zsh
+++ migrate.zsh
@@ -4,35 +4,7 @@ set -o verbose
 
 # migrate
 
-sudo pacman --noconfirm -Syu
-
-sudo pacman -S --noconfirm \
-  glib2-devel
-
-sudo pacman -S --noconfirm \
-  gopass
-
-sudo pacman -Rs --noconfirm \
-  cava
-
-paru -S --aur --noconfirm \
-  cava
-
-sudo pacman -S --noconfirm \
-  ffmpegthumbnailer \
-  imagemagick \
-  ouch \
-  yazi
-
-sudo sed -i -e '/.*local.*/d' /etc/hosts
-
-echo '127.0.0.1 localhost' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       localhost' | sudo tee --append /etc/hosts > /dev/null
-
-echo '127.0.0.1 player.localdomain player' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       player.localdomain player' | sudo tee --append /etc/hosts > /dev/null
-
-. `dirname $0`/apsis.zsh
+sudo cp `dirname $0`/etc/tmpfiles.d/hibernation_image_size.conf /etc/tmpfiles.d/hibernation_image_size.conf
 
 # cleanup
 
