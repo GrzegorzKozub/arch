9e8dddee0156c6a3970c677bace7e0a13c67c5dd
greg <grzegorz.kozub@gmail.com>
2024-07-15 08:45:01 +0200

    migrate cleanup

--- migrate.zsh
+++ migrate.zsh
@@ -4,32 +4,8 @@ set -o verbose
 
 # migrate
 
-sudo pacman -S --noconfirm \
-  git-credential-gopass \
-  gnome-characters
-
-rm $XDG_DATA_HOME/applications/satty.desktop
-
-sudo pacman -Rs --noconfirm \
-  foot
-
-for APP in \
-  org.codeberg.dnkl.foot-server \
-  org.codeberg.dnkl.foot \
-  org.codeberg.dnkl.footclient
-do
-  rm $XDG_DATA_HOME/applications/$APP.desktop
-done
-
-rm -rf $XDG_CONFIG_HOME/foot
-
-gsettings set org.gnome.desktop.interface cursor-size 24
-
 # cleanup
 
 . `dirname $0`/packages.zsh
 . `dirname $0`/clean.zsh
 
-# manual
-
-# vrr
