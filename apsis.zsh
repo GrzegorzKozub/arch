1d247a95e96b27223889ffd0a6fcdf2fd3b5c7ef
greg <grzegorz.kozub@gmail.com>
2024-07-21 07:41:10 +0200

    refac

--- apsis.zsh
+++ apsis.zsh
@@ -4,19 +4,18 @@ set -o verbose
 
 # hosts
 
-sudo sed -i -e '/.*apsis.*/d' -e '/.*stage.*/d' -e '/.*amazonaws.*/d' /etc/hosts
+HOSTS=(
+  'int.aud-stage.apsis.cloud'
+  'alb-int.aud-stage.apsis.cloud'
+  'api.stage.ma'
+  'dev.apsis'
+  'ANUNLB-LB-VC5YGOQNTGG4-7707d28b7ca45024.elb.eu-west-1.amazonaws.com'
+)
 
-echo '127.0.0.1 int.aud-stage.apsis.cloud' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       int.aud-stage.apsis.cloud' | sudo tee --append /etc/hosts > /dev/null
+for HOST in $HOSTS; do
+  sudo sed -i -e "/.*$HOST.*/d" /etc/hosts
+done
 
-echo '127.0.0.1 alb-int.aud-stage.apsis.cloud' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       alb-int.aud-stage.apsis.cloud' | sudo tee --append /etc/hosts > /dev/null
-
-echo '127.0.0.1 api.stage.ma' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       api.stage.ma' | sudo tee --append /etc/hosts > /dev/null
-
-echo '127.0.0.1 dev.apsis' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       dev.apsis' | sudo tee --append /etc/hosts > /dev/null
-
-echo '127.0.0.1 ANUNLB-LB-VC5YGOQNTGG4-7707d28b7ca45024.elb.eu-west-1.amazonaws.com' | sudo tee --append /etc/hosts > /dev/null
-echo '::1       ANUNLB-LB-VC5YGOQNTGG4-7707d28b7ca45024.elb.eu-west-1.amazonaws.com' | sudo tee --append /etc/hosts > /dev/null
+for HOST in $HOSTS; do
+  echo "127.0.0.1 $HOST\n::1       $HOST" | sudo tee --append /etc/hosts > /dev/null
+done
