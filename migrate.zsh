484f5ce9db6a6bc1b0ade29fd49ebd20edb983ad
greg <grzegorz.kozub@gmail.com>
2024-07-28 11:48:27 +0200

    temp migrate split

--- migrate.zsh
+++ migrate.zsh
@@ -46,21 +46,12 @@ popd
 paru -S --aur --noconfirm \
   vivify-bin
 
-# migrate next
-
 sudo pacman -S --noconfirm \
   realtime-privileges
 
 sudo usermod -a -G realtime greg
 
 sudo cp `dirname $0`/etc/tmpfiles.d/rtc.conf /etc/tmpfiles.d
-
-if [[ $MY_HOSTNAME = 'player' ]]; then
-
-  sudo rm /etc/modprobe.d/nvidia-power-management.conf
-  echo 'options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp' | sudo tee /etc/modprobe.d/nvidia.conf > /dev/null
-
-fi
 
 # cleanup
 
