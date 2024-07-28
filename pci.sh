70abb136fd66ae3a148aa6d4a190480339ad01eb
greg <grzegorz.kozub@gmail.com>
2024-07-28 11:27:59 +0200

    fix pci service

--- pci.sh
+++ pci.sh
@@ -4,8 +4,8 @@ set -e
 
 # https://wiki.archlinux.org/title/Gaming#Improve_PCI_Express_Latencies
 
-setpci -v -s '*:*' latency_timer=20
-setpci -v -s '0:0' latency_timer=0
+sudo setpci -v -s '*:*' latency_timer=20
+sudo setpci -v -s '0:0' latency_timer=0
 
-setpci -v -d "*:*:04xx" latency_timer=80
+sudo setpci -v -d "*:*:04xx" latency_timer=80
 
