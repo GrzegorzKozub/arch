028100b09426f8006640213b7fb37693d85913c5
greg <grzegorz.kozub@gmail.com>
2024-07-28 07:09:29 +0200

    remove rimraf & express-generator

--- migrate.zsh
+++ migrate.zsh
@@ -7,6 +7,18 @@ set -o verbose
 paru -S --aur --noconfirm \
   gnome-shell-extension-rounded-window-corners-reborn
 
+rm -rf ~/.cache/npm
+
+fnm uninstall 22
+fnm install --latest
+fnm use default
+
+npm install --global \
+  autocannon \
+  eslint \
+  neovim \
+  typescript
+
 # cleanup
 
 . `dirname $0`/packages.zsh
