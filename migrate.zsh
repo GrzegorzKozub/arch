2c501aff9e3d24352ab28ef80a3fb08e58c51d3c
greg <grzegorz.kozub@gmail.com>
2024-07-29 08:16:24 +0200

    reset migrate

--- migrate.zsh
+++ migrate.zsh
@@ -4,55 +4,6 @@ set -o verbose
 
 # migrate
 
-paru -S --aur --noconfirm \
-  gnome-shell-extension-rounded-window-corners-reborn
-
-rm -rf ~/.cache/npm
-
-fnm uninstall 22
-fnm install --latest
-fnm use default
-
-npm install --global \
-  autocannon \
-  eslint \
-  neovim \
-  typescript
-
-paru -S --aur --noconfirm \
-  teams-for-linux
-
-cp /usr/share/applications/teams-for-linux.desktop $XDG_DATA_HOME/applications
-sed -i -e 's/^Name=.*/Name=Teams/' $XDG_DATA_HOME/applications/teams-for-linux.desktop
-
-if [[ $XDG_SESSION_TYPE = 'wayland' ]]; then
-
-  sed -i \
-    -e 's/^Exec=teams-for-linux/Exec=teams-for-linux --disable-features=WaylandFractionalScaleV1 --enable-features=WaylandWindowDecorations --ozone-platform-hint=auto/' \
-    $XDG_DATA_HOME/applications/teams-for-linux.desktop
-
-fi
-
-sudo pacman -Rs --noconfirm \
-  node-gyp nodejs npm
-
-pushd ~/code/dotfiles
-
-stow --dir=`dirname $0` --target=$XDG_CONFIG_HOME --stow \
-  teams-for-linux
-
-popd
-
-paru -S --aur --noconfirm \
-  vivify-bin
-
-sudo pacman -S --noconfirm \
-  realtime-privileges
-
-sudo usermod -a -G realtime greg
-
-sudo cp `dirname $0`/etc/tmpfiles.d/rtc.conf /etc/tmpfiles.d
-
 # cleanup
 
 . `dirname $0`/packages.zsh
