2cb17bd082c1795837ac9466aa10053c0e79d760
greg <grzegorz.kozub@gmail.com>
2024-08-14 07:29:39 +0200

    yazi migration

--- migrate.zsh
+++ migrate.zsh
@@ -7,6 +7,19 @@ set -o verbose
 paru -S --aur \
   tmux-git
 
+sudo pacman -Rs --noconfirm \
+  ouch
+
+FILE=$XDG_CONFIG_HOME/yazi/package.toml
+[[ -f $FILE ]] && rm -f $FILE
+
+for PLUGIN in \
+  KKV9/compress \
+  yazi-rs/plugins#jump-to-char
+do
+  ya pack --add "$PLUGIN"
+done
+
 # cleanup
 
 . `dirname $0`/packages.zsh
