3dd07e823d9c1f362610f5a92e7eb22ef68d77f8
greg <grzegorz.kozub@gmail.com>
2024-08-19 20:47:30 +0200

    migrate reset

--- migrate.zsh
+++ migrate.zsh
@@ -4,35 +4,6 @@ set -o verbose
 
 # migrate
 
-FILE=/etc/tmpfiles.d/energy_performance_preference.conf
-[[ -f $FILE ]] && sudo rm -f $FILE
-
-sudo pacman -S --noconfirm \
-  fwupd
-
-set +e
-sudo pacman -Rs --noconfirm \
-  ouch
-set -e
-
-set +e
-sudo pacman -Rs --noconfirm \
-  yazi
-set -e
-
-paru -S --aur --noconfirm \
-  yazi-nightly-bin
-
-FILE=$XDG_CONFIG_HOME/yazi/package.toml
-[[ -f $FILE ]] && rm -f $FILE
-
-for PLUGIN in \
-  KKV9/compress \
-  yazi-rs/plugins#jump-to-char
-do
-  ya pack --add "$PLUGIN"
-done
-
 # cleanup
 
 . `dirname $0`/packages.zsh
