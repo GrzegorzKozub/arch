f6bcb9db50669e5e8adf67ff7e73a26366c857fa
greg <grzegorz.kozub@gmail.com>
2024-08-23 08:49:03 +0200

    refactor vpn

--- vpn.zsh
+++ vpn.zsh
@@ -17,26 +17,35 @@ sudo pacman -S --noconfirm \
 
 # sudo systemctl enable systemd-resolved.service
 
-# apsis
+# connections
 
-[[ $(nmcli connection | grep apsis) ]] && nmcli connection delete apsis
+DIR=~/code/keys/openvpn
+CONN=apsis
 
-nmcli connection import type openvpn file ~/code/keys/openvpn/apsis.ovpn
+[[ $(nmcli connection | grep $CONN) ]] && nmcli connection delete $CONN
 
-nmcli connection modify apsis +vpn.data username=grko_vpn
-nmcli connection modify apsis +vpn.data password-flags=2
+nmcli connection import type openvpn file $DIR/$CONN.ovpn
 
-nmcli connection modify apsis ipv4.dns-search 'apsis.local'
+nmcli connection modify $CONN +vpn.data username=grko_vpn
+nmcli connection modify $CONN +vpn.data password-flags=2
 
-nmcli connection modify apsis ipv4.never-default 'yes'
-nmcli connection modify apsis ipv6.never-default 'yes'
+nmcli connection modify $CONN ipv4.dns-search 'apsis.local'
+
+nmcli connection modify $CONN ipv4.never-default 'yes'
+nmcli connection modify $CONN ipv6.never-default 'yes'
+
+for FILE in $DIR/*.ovpn; do
+
+  CONN=$(basename -- $FILE .ovpn)
+
+  [[ $CONN = 'apsis' ]] && continue
+
+  [[ $(nmcli connection | grep $CONN) ]] && nmcli connection delete $CONN
+
+  nmcli connection import type openvpn file $FILE
+  nmcli connection modify $CONN +vpn.data password-flags=2
+
+done
 
 # nmcli connection up apsis --ask
 
-# webscript-stage
-
-[[ $(nmcli connection | grep webscript-stage) ]] && nmcli connection delete webscript-stage
-
-nmcli connection import type openvpn file ~/code/keys/openvpn/webscript-stage.ovpn
-
-nmcli connection modify webscript-stage +vpn.data password-flags=2
