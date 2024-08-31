73d65844afcb9d76a2e9cdf6653b4ba109c4258d
greg <grzegorz.kozub@gmail.com>
2024-08-31 08:04:35 +0200

    reset migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,17 +2,6 @@
 
 set -o verbose
 
-# firmware
-
-sudo pacman -S --noconfirm linux-firmware-qlogic
-paru -S --aur --noconfirm ast-firmware
-
-# dm-crypt
-
-sudo sed -i \
-  's/discard$/allow-discards,no-read-workqueue,no-write-workqueue,tpm2-device=auto/' \
-  /etc/crypttab.initramfs
-
 # cleanup
 
 . `dirname $0`/packages.zsh
