6ec2f008ceeee10d7fc29523e8ba7649452407db
greg <grzegorz.kozub@gmail.com>
2024-11-19 06:57:21 +0100

    migrate reset

--- migrate.zsh
+++ migrate.zsh
@@ -4,25 +4,6 @@ set -o verbose
 
 # cleanup
 
-rm -f $XDG_DATA_HOME/applications/electron30.desktop
-rm -f $XDG_DATA_HOME/applications/electron31.desktop
-rm -f $XDG_DATA_HOME/applications/redshift-gtk.desktop
-rm -f $XDG_DATA_HOME/applications/redshift.desktop
-
-rm -f $XDG_DATA_HOME/mimeapps.list
-
-rm -f $SDG_CONFIG_HOME/monitors.xml\~
-
-# shellcheck
-
-paru -S --aur --noconfirm shellcheck-bin
-
-# vscode
-
-code --uninstall-extension antfu.icons-carbon
-
-# cleanup
-
 . `dirname $0`/packages.zsh
 . `dirname $0`/clean.zsh
 
