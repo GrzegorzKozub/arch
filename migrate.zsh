05eb9f52d6541cac4ef1947e0b28e9e007ce7a4e
greg <grzegorz.kozub@gmail.com>
2024-11-30 08:04:54 +0100

    migrate yazi

--- migrate.zsh
+++ migrate.zsh
@@ -6,6 +6,21 @@ set -o verbose
 
 bat cache --build
 
+# yazi
+
+rm -f ~/.config/yazi/package.toml
+rm -rf ~/.config/yazi/plugins/video-ffmpeg
+rm -rf ~/.local/state/yazi
+
+for PLUGIN in \
+  GrzegorzKozub/mdcat \
+  KKV9/compress \
+  yazi-rs/plugins:git \
+  yazi-rs/plugins:jump-to-char
+do
+  ya pack --add "$PLUGIN"
+done
+
 # cleanup
 
 . `dirname $0`/packages.zsh
