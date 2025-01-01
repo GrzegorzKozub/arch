5ca48c02816378f686fab2fa5d0fe68daa0503f4
greg <grzegorz.kozub@gmail.com>
2025-01-01 15:08:29 +0100

    migrate bat & silicon

--- migrate.zsh
+++ migrate.zsh
@@ -7,6 +7,14 @@ set -o verbose
 paru -Rs --aur --noconfirm 7-zip-full
 sudo pacman -S --noconfirm 7zip
 
+# bat
+
+bat cache --build
+
+pushd $XDG_CONFIG_HOME/silicon
+silicon --build-cache
+popd
+
 # cleanup
 
 . `dirname $0`/packages.zsh
