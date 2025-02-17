8d8491f3a5b6506477033c2e9bca8e6db8a21f7f
greg <grzegorz.kozub@gmail.com>
2025-02-17 08:24:33 +0100

    migrate reset

--- migrate.zsh
+++ migrate.zsh
@@ -2,34 +2,6 @@
 
 set -o verbose
 
-# debug
-
-sudo sed -i 's/^OPTIONS=\(.*\) debug\(.*\)$/OPTIONS=\1 !debug\2/' /etc/makepkg.conf
-
-# icons
-
-rm -rf ~/.local/share/icons
-
-sudo pacman -Rs --noconfirm papirus-icon-theme
-paru -S --aur --needed --noconfirm papirus-icon-theme-git
-
-# obsidian
-
-sudo pacman -Rs --noconfirm electron32
-rm ~/.local/share/applications/electron32.desktop
-
-# refine
-
-paru -S --aur --noconfirm refine
-
-# xh
-
-sudo pacman -S --noconfirm xh
-
-# yazi
-
-ya pack --delete yazi-rs/plugins:jump-to-char
-
 # cleanup
 
 . `dirname $0`/packages.zsh
