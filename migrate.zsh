0cc4bb005961d80721922cafafce48d49c0bd13c
greg <grzegorz.kozub@gmail.com>
2025-03-04 20:40:14 +0100

    tmux refinement

--- migrate.zsh
+++ migrate.zsh
@@ -6,6 +6,13 @@ set -o verbose
 
 sudo pacman -S --noconfirm ripgrep-all
 
+# tmux
+
+rm -rf $XDG_DATA_HOME/tmux/plugins/tmux-battery
+rm -rf $XDG_DATA_HOME/tmux/resurrect/*
+
+$XDG_DATA_HOME/tmux/plugins/tpm/bindings/install_plugins
+
 # cleanup
 
 . `dirname $0`/packages.zsh
