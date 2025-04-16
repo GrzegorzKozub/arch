564d9da0553b971ad0cc44667fc35c696f274e3b
greg <grzegorz.kozub@gmail.com>
2025-04-16 06:52:01 +0200

    reset nvim

--- migrate.zsh
+++ migrate.zsh
@@ -6,6 +6,16 @@ set -o verbose
 
 sudo pacman -S --noconfirm ghostty
 
+# nvim
+
+rm -rf $XDG_CACHE_HOME/nvim
+rm -rf $XDG_DATA_HOME/nvim
+rm -rf ~/.local/state/nvim
+
+nvim \
+  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+  -c 'autocmd User VeryLazy MasonToolsUpdate'
+
 # vconsole
 
 [[ $HOST = 'player' ]] &&
