160e60f18dc61c7d0efcaa68a631e193de974b74
greg <grzegorz.kozub@gmail.com>
2025-05-07 08:01:33 +0200

    fix obsidian migration

--- migrate.zsh
+++ migrate.zsh
@@ -2,7 +2,19 @@
 
 set -o verbose
 
+# nvim
+
+rm -rf $XDG_CACHE_HOME/nvim
+rm -rf $XDG_DATA_HOME/nvim
+rm -rf ~/.local/state/nvim
+
+nvim \
+  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+  -c 'autocmd User VeryLazy MasonToolsUpdate'
+
 # obsidian
+
+pushd ~/code/dot && git pull && popd
 
 [[ -f $XDG_CONFIG_HOME/obsidian/user-flags.conf ]] || {
 
