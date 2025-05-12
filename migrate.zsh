d8b60beda0d059821f21fee20123cbf6e7e09184
greg <grzegorz.kozub@gmail.com>
2025-05-12 19:38:06 +0200

    reset migration

--- migrate.zsh
+++ migrate.zsh
@@ -2,48 +2,15 @@
 
 set -o verbose
 
-# btop
-
-pushd ~/code/dot
-git update-index --assume-unchanged btop/btop/btop.conf
-popd
-
 # nvim
 
-rm -rf $XDG_CACHE_HOME/nvim
-rm -rf $XDG_DATA_HOME/nvim
-rm -rf ~/.local/state/nvim
-
-nvim \
-  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
-  -c 'autocmd User VeryLazy MasonToolsUpdate'
-
-# obsidian
-
-pushd ~/code/dot && git pull && popd
-
-[[ -f $XDG_CONFIG_HOME/obsidian/user-flags.conf ]] || {
-
-  mv $XDG_CONFIG_HOME/obsidian ~/Downloads
-
-  pushd ~/code/dot
-  stow --dir=`dirname $0` --target=$XDG_CONFIG_HOME --stow obsidian
-  popd
-
-  mv ~/Downloads/obsidian/* $XDG_CONFIG_HOME/obsidian/
-
-  rm -rf ~/Downloads/obsidian
-  rm -f $XDG_DATA_HOME/applications/obsidian.desktop
-}
-
-# papers
-
-sudo pacman --noconfirm -R evince
-sudo pacman --noconfirm -S papers
-
-# pastel
-
-sudo pacman -S --noconfirm pastel
+# rm -rf $XDG_CACHE_HOME/nvim
+# rm -rf $XDG_DATA_HOME/nvim
+# rm -rf ~/.local/state/nvim
+#
+# nvim \
+#   -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+#   -c 'autocmd User VeryLazy MasonToolsUpdate'
 
 # cleanup
 
