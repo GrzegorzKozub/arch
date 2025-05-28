6cee5cc338957800ec2c2853df8133b622883d8f
greg <grzegorz.kozub@gmail.com>
2025-05-28 06:47:10 +0200

    aws-cli-v2 workaround for https://github.com/aws/aws-cli/issues/9453

--- migrate.zsh
+++ migrate.zsh
@@ -2,15 +2,19 @@
 
 set -o verbose
 
+# aws-cli-v2
+
+sudo sed -i -e 's/^IgnorePkg.*$/#IgnorePkg   =/' /etc/pacman.conf # aws-cli-v2 python-prompt_toolkit
+
 # nvim
 
-# rm -rf $XDG_CACHE_HOME/nvim
-# rm -rf $XDG_DATA_HOME/nvim
-# rm -rf ~/.local/state/nvim
-#
-# nvim \
-#   -c 'autocmd User MasonToolsUpdateCompleted quitall' \
-#   -c 'autocmd User VeryLazy MasonToolsUpdate'
+rm -rf $XDG_CACHE_HOME/nvim
+rm -rf $XDG_DATA_HOME/nvim
+rm -rf ~/.local/state/nvim
+
+nvim \
+  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+  -c 'autocmd User VeryLazy MasonToolsUpdate'
 
 # cleanup
 
