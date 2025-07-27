cf7f6d1ce6dff0fe46800f74e35b7a580c7f153e
greg <grzegorz.kozub@gmail.com>
2025-07-27 14:13:52 +0200

    reset neovim on next migrate

--- migrate.zsh
+++ migrate.zsh
@@ -12,12 +12,12 @@ set -o verbose
 
 # nvim
 
-# rm -rf $XDG_CACHE_HOME/nvim
-# rm -rf $XDG_DATA_HOME/nvim
-# rm -rf ~/.local/state/nvim
-# nvim \
-#   -c 'autocmd User MasonToolsUpdateCompleted quitall' \
-#   -c 'autocmd User VeryLazy MasonToolsUpdate'
+rm -rf $XDG_CACHE_HOME/nvim
+rm -rf $XDG_DATA_HOME/nvim
+rm -rf ~/.local/state/nvim
+nvim \
+  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+  -c 'autocmd User VeryLazy MasonToolsUpdate'
 
 # cleanup
 
