6a360b257bb510c42b3b0331678e6f925a2cd14d
greg <grzegorz.kozub@gmail.com>
2025-07-28 08:59:40 +0200

    reset migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,35 +2,14 @@
 
 set -o verbose
 
-# lact
-
-if [[ $HOST =~ ^(player|worker)$ ]]; then
-
-  sudo pacman -S --noconfirm lact
-  sudo systemctl enable --now lactd.service
-
-fi
-
-# nvidia
-
-if [[ $HOST =~ ^(player|worker)$ ]]; then
-
-  sudo pacman --noconfirm -Rs nvidia nvidia-lts
-  sudo pacman --noconfirm -S nvidia-open nvidia-open-lts
-
-  sudo mkinitcpio -p linux
-  sudo mkinitcpio -p linux-lts
-
-fi
-
 # nvim
 
-rm -rf $XDG_CACHE_HOME/nvim
-rm -rf $XDG_DATA_HOME/nvim
-rm -rf ~/.local/state/nvim
-nvim \
-  -c 'autocmd User MasonToolsUpdateCompleted quitall' \
-  -c 'autocmd User VeryLazy MasonToolsUpdate'
+# rm -rf $XDG_CACHE_HOME/nvim
+# rm -rf $XDG_DATA_HOME/nvim
+# rm -rf ~/.local/state/nvim
+# nvim \
+#   -c 'autocmd User MasonToolsUpdateCompleted quitall' \
+#   -c 'autocmd User VeryLazy MasonToolsUpdate'
 
 # cleanup
 
