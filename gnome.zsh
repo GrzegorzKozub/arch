fb43e4a92a45ea239ef6cd23a3447174393938b2
greg <grzegorz.kozub@gmail.com>
2025-08-06 08:48:07 +0200

    ctrl+prtscrn for a delayed screenshot

--- gnome.zsh
+++ gnome.zsh
@@ -296,8 +296,9 @@ add_shortcut() {
 }
 
 add_shortcut 0 'Print' 'screenshot' "/home/$USER/code/arch/screenshot.zsh"
-add_shortcut 1 '<Control><Super>a' 'audio output' "/home/$USER/code/arch/audio.zsh sink"
-add_shortcut 2 '<Control><Super>m' 'audio input' "/home/$USER/code/arch/audio.zsh source"
+add_shortcut 1 '<Control>Print' 'screenshot delay' "/home/$USER/code/arch/screenshot.zsh delay"
+add_shortcut 2 '<Control><Super>a' 'audio output' "/home/$USER/code/arch/audio.zsh sink"
+add_shortcut 3 '<Control><Super>m' 'audio input' "/home/$USER/code/arch/audio.zsh source"
 
 # [[ $HOST =~ ^(player|worker)$ ]] &&
 #   add_shortcut 3 '<Control><Super>n' 'night light' 'pkill -USR1 redshift'
--- screenshot.zsh
+++ screenshot.zsh
@@ -5,9 +5,10 @@ set -e
 # screenshot
 
 [[ $HOST = 'drifter' ]] && RESIZE=33 || RESIZE=50
+[[ $1 = 'delay' ]] && DELAY=3 || DELAY=0
 
 FILE=/tmp/screenshot.png
-gnome-screenshot --area --file $FILE # --delay 3
+gnome-screenshot --area --file $FILE --delay $DELAY
 magick $FILE -filter lanczos -resize $RESIZE% -unsharp 0x0.75 $FILE
 satty --filename $FILE
 rm $FILE
