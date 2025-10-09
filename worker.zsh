2f2438666a560a211e72de6ec936585671b1fe09
greg <grzegorz.kozub@gmail.com>
2025-10-09 07:53:01 +0200

    update worker efi partition number

--- worker.zsh
+++ worker.zsh
@@ -4,7 +4,7 @@
 
 export MY_DISK=/dev/nvme0n1
 
-export MY_EFI_PART=/dev/nvme0n1p2
+export MY_EFI_PART=/dev/nvme0n1p1
 export MY_EFI_PART_NBR=${MY_EFI_PART: -1}
 export MY_ARCH_PART=/dev/nvme0n1p5
 
