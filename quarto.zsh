a30142e5c4948b01cd4321e8e245419127a91423
greg <grzegorz.kozub@gmail.com>
2025-10-14 12:34:28 +0200

    audience vpn

--- quarto.zsh
+++ quarto.zsh
@@ -13,7 +13,20 @@ pip install --user --break-system-packages \
 paru -S --aur --noconfirm \
   quarto-cli-bin
 
+# links
+
+for APP in \
+  ipython \
+  jupyterlab
+do
+  cp /usr/share/applications/$APP.desktop $XDG_DATA_HOME/applications
+  sed -i '2iNoDisplay=true' $XDG_DATA_HOME/applications/$APP.desktop
+done
+
 # cleanup
 
 . `dirname $0`/packages.zsh
 
+# dotfiles
+
+[[ -d $XDG_CONFIG_HOME/ipython ]] || mkdir $XDG_CONFIG_HOME/ipython
--- vpn.zsh
+++ vpn.zsh
@@ -27,8 +27,40 @@ if [[ $1 == 'audience' ]]; then
   # packages
 
   sudo pacman -S --noconfirm \
-    networkmanager-strongswan \
-    strongswan
+    libnma \
+    strongswan \
+    networkmanager-strongswan
+
+    # nm-connection-editor
+
+  # connections
+
+  DIR=~/code/keys/strongswan
+
+  typeset -A ENVS=(
+    'stage' 'ec2-54-217-117-208.eu-west-1.compute.amazonaws.com'
+  )
+
+  for ENV ADDR ("${(@kv)ENVS}"); do
+
+    CONN="audience-$ENV"
+
+    [[ $(nmcli connection | grep "$CONN ") ]] && nmcli connection delete $CONN
+
+    nmcli connection add type vpn vpn-type strongswan con-name $CONN
+
+    nmcli connection modify $CONN +vpn.data address=$ADDR
+    nmcli connection modify $CONN +vpn.data certificate=$DIR/$CONN-ca.pem
+
+    nmcli connection modify $CONN +vpn.data method=cert
+    nmcli connection modify $CONN +vpn.data usercert=$DIR/$CONN-cert.pem
+    nmcli connection modify $CONN +vpn.data userkey=$DIR/$CONN-key.pem
+
+    nmcli connection modify $CONN +vpn.data virtual=yes
+
+    nmcli connection modify $CONN ipv4.dns '10.103.11.212, 8.8.8.8'
+
+  done
 
 fi
 
