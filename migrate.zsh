015e852587e79fb6e238a2c70ca1c03a0f7c9186
greg <grzegorz.kozub@gmail.com>
2025-10-28 08:42:17 +0100

    remove intune

--- migrate.zsh
+++ migrate.zsh
@@ -2,28 +2,6 @@
 
 set -o verbose
 
-# bat
-
-bat cache --build
-
-# vscode
-
-if [[ $HOST =~ ^(drifter|worker)$ ]]; then # work
-
-  for EXTENSION in \
-    github.copilot \
-    github.copilot-chat \
-    redhat.java
-  do
-    code --install-extension $EXTENSION --force
-  done
-
-fi
-
-# uv
-
-sudo pacman -S --noconfirm uv
-
 # reset
 
 # . `dirname $0`/reset.zsh
