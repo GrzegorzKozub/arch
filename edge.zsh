bc434ecbbf34cb38ff28537c00469bf2df0e9d1d
greg <grzegorz.kozub@gmail.com>
2025-11-02 09:43:00 +0100

    allow hiding copilot on edge

--- edge.zsh
+++ edge.zsh
@@ -7,6 +7,11 @@ set -e -o verbose
 paru -S --aur --noconfirm \
   microsoft-edge-stable-bin
 
+# https://github.com/MicrosoftEdge/DevTools/issues/278
+DIR=microsoft-edge/Default
+[[ -d $XDG_CONFIG_HOME/$DIR ]] || mkdir -p $XDG_CONFIG_HOME/$DIR
+cp `dirname $0`/home/$USER/.config/$DIR/HubApps $XDG_CONFIG_HOME/$DIR/
+
 # links
 
 cp /usr/share/applications/microsoft-edge.desktop $XDG_DATA_HOME/applications
