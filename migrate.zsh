9640b6d2962f19f82593db443e529d9fb7adcb3c
greg <grzegorz.kozub@gmail.com>
2025-11-03 19:43:08 +0100

    reset migrate

--- migrate.zsh
+++ migrate.zsh
@@ -2,22 +2,6 @@
 
 set -o verbose
 
-# java
-
-if [[ $HOST =~ ^(drifter|worker)$ ]]; then # work
-
-  sudo pacman -S --noconfirm jdk-openjdk maven
-
-  pushd ~/code/dot
-  git update-index --assume-unchanged maven/maven/settings.xml
-  popd
-
-fi
-
-# seahorse
-
-sudo pacman -S --noconfirm seahorse
-
 # reset
 
 # . `dirname $0`/reset.zsh
