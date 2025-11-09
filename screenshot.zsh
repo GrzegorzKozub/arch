02b59df24767fbf99d7e8380f82b13af4961c93b
greg <grzegorz.kozub@gmail.com>
2025-11-09 21:22:30 +0100

    attempt first screenshot fix

--- screenshot.zsh
+++ screenshot.zsh
@@ -7,10 +7,13 @@ set -e
 [[ $HOST = 'drifter' ]] && RESIZE=33 || RESIZE=50
 
 fswatch --one-event --event Created ~/Pictures/Screenshots | while read FILE; do
+  FILE="$DIR$FILE"
   magick $FILE -filter lanczos -resize $RESIZE% -unsharp 0x0.75 $FILE
   satty --filename $FILE >/dev/null 2>&1
   rm $FILE
 done &
+
+sleep 0.25
 
 gdbus call \
   --session \
@@ -22,3 +25,4 @@ gdbus call \
 
 # consider turning into a gnome shell extension based on
 # https://github.com/OttoAllmendinger/gnome-shell-screenshot/
+# https://github.com/AlexanderVanhee/Gradia
