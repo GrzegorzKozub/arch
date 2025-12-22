6c2bb3ffdf28ee311d23f7f5ca35490a5cdb4821
greg <grzegorz.kozub@gmail.com>
2025-12-22 09:18:06 +0100

    take a note of the weird pacman download trash

--- migrate.zsh
+++ migrate.zsh
@@ -3,6 +3,9 @@
 set -o verbose
 
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
+#
+# pacman bug: https://forum.endeavouros.com/t/solved-latest-pacman-update-breaks-aur-and-yay/76959/92
+# sudo rm -rf /var/cache/pacman/pkg/download-*
 
 if [[ $HOST = 'worker' ]]; then
 
