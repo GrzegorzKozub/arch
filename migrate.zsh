c3de76dd39fdc6018879d98258d47b390d1f6bd2
greg <grzegorz.kozub@gmail.com>
2025-12-30 08:45:24 +0100

    no aws on player

--- migrate.zsh
+++ migrate.zsh
@@ -4,6 +4,17 @@ set -o verbose
 
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
 
+# secrets
+
+if [[ $HOST = 'player' ]]; then
+
+  rm -rf ~/.config/aws
+  pushd ~/code/keys
+  git update-index --assume-unchanged aws/aws/credentials
+  popd
+
+fi
+
 # cleanup
 
 . `dirname $0`/packages.zsh
