dfd08f89ba267ed4a8280c84101015e7d01dc412
greg <grzegorz.kozub@gmail.com>
2026-01-06 10:21:47 +0100

    migrate update to sh

--- migrate.sh
+++ migrate.sh
@@ -1,31 +1,30 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-set -o verbose
+set -e -o verbose
 
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
 
-# archiso
-
-for FILE in \
-  archiso/vmlinuz-linux \
-  archiso/vmlinuz-linux-lts
-do
-  sudo test -f /boot/$FILE || continue
-  sudo sbctl sign --save /boot/$FILE
-done
-
-# gnome terminal
-
-sudo pacman -Rs gnome-terminal
-dconf reset -f '/org/gnome/terminal/'
-rm -f ~/.local/share/applications/org.gnome.Terminal.desktop
-
-# env
-
-sudo cp $(dirname $0)/etc/zsh/zshenv /etc/zsh
-rm -f ~/.zprofile
-
 if [[ $HOST == 'worker' ]]; then
+
+  # archiso
+
+  for FILE in \
+    archiso/vmlinuz-linux \
+    archiso/vmlinuz-linux-lts; do
+    sudo test -f /boot/$FILE || continue
+    sudo sbctl sign --save /boot/$FILE
+  done
+
+  # gnome terminal
+
+  sudo pacman -Rs gnome-terminal
+  dconf reset -f '/org/gnome/terminal/'
+  rm -f ~/.local/share/applications/org.gnome.Terminal.desktop
+
+  # env
+
+  sudo cp "${BASH_SOURCE%/*}"/etc/zsh/zshenv /etc/zsh
+  rm -f ~/.zprofile
 
   # work
 
@@ -57,16 +56,16 @@ if [[ $HOST == 'worker' ]]; then
   BOOTNUM=$(efibootmgr | grep 'Linux Boot Manager' | awk '{print $1}' | grep -o '[0-9]*' || true)
   [[ $BOOTNUM ]] && sudo efibootmgr --delete-bootnum --bootnum "$BOOTNUM"
 
-  `dirname $0`/preloader.sh disable
+  "${BASH_SOURCE%/*}"/preloader.sh disable
 
   # limine
 
   [[ -d /etc/pacman.d/hooks ]] || sudo mkdir -p /etc/pacman.d/hooks
-  sudo cp $(dirname $0)/etc/pacman.d/hooks/90-limine-update.hook /etc/pacman.d/hooks/
+  sudo cp "${BASH_SOURCE%/*}"/etc/pacman.d/hooks/90-limine-update.hook /etc/pacman.d/hooks/
 
   sudo pacman -S --noconfirm limine
 
-  sudo cp $(dirname $0)/boot/EFI/limine/limine.conf /boot/EFI/limine/
+  sudo cp "${BASH_SOURCE%/*}"/boot/EFI/limine/limine.conf /boot/EFI/limine/
   sudo cp ~/code/walls/women.jpg /boot/EFI/limine/wall.jpg
 
   sudo sed -i "s/<host>/$HOST/g" /boot/EFI/limine/limine.conf
@@ -98,16 +97,15 @@ if [[ $HOST == 'worker' ]]; then
   BOOTNUM=$(efibootmgr | grep 'limine' | awk '{print $1}' | grep -o '[0-9]*' || true)
   [[ $BOOTNUM ]] && sudo efibootmgr --delete-bootnum --bootnum "$BOOTNUM"
 
-  . `dirname $0`/$HOST.zsh
+  . "${BASH_SOURCE%/*}"/"$HOST".zsh
 
   sudo efibootmgr --create --label 'limine' \
-    --disk $MY_DISK --part $MY_EFI_PART_NBR --loader /EFI/limine/liminex64.efi \
+    --disk "$MY_DISK" --part "$MY_EFI_PART_NBR" --loader /EFI/limine/liminex64.efi \
     --unicode
 
 fi
 
 # cleanup
 
-`dirname $0`/packages.zsh
-`dirname $0`/clean.zsh
-
+"${BASH_SOURCE%/*}"/packages.zsh
+"${BASH_SOURCE%/*}"/clean.zsh
