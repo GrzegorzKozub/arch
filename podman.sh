2c8ab46fcab6eab836abf01bef8b4365fda03273
greg <grzegorz.kozub@gmail.com>
2026-01-11 09:07:27 +0100

    init podman script

---
+++ podman.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e -o verbose
+
+# packages
+
+sudo pacman -S --noconfirm \
+  podman
+
+  # podman-compose (if docker-compose is not installed), podman-docker
+
+# to be continued
+# https://wiki.archlinux.org/title/Podman
+#https://github.com/containers/podman/tree/main/docs/tutorials
