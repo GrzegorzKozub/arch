ebc03a6ee69a5a5aeae52c33cabb517216906250
greg <grzegorz.kozub@gmail.com>
2026-01-26 09:18:28 +0100

    migrate cleanup

--- migrate.sh
+++ migrate.sh
@@ -3,51 +3,18 @@ set -eo pipefail -ux
 
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
 
-# coredump
+# jack
 
-sudo rm -f /etc/tmpfiles.d/coredump.conf
+set +e
+yes | sudo pacman -S pipewire-jack
+set -e
 
-# docker & podman
+# noise-suppression-for-voice
 
-sudo sed -i '/231072/d' /etc/subgid /etc/subuid
-# sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 "$USER"
-
-"${BASH_SOURCE%/*}"/podman.sh
-
-# hosts
-
-[[ $HOST == 'worker' ]] && "${BASH_SOURCE%/*}"/hosts.zsh
-
-# limine
-
-sudo rm -f /etc/pacman.d/hooks/90-limine-update.hook
-sudo cp "${BASH_SOURCE%/*}"/etc/pacman.d/hooks/91-limine.hook /etc/pacman.d/hooks/
-
-# vscode
-
-code --install-extension ms-vscode-remote.remote-containers --force
-
-# webcam
-
-sudo rm -rf /etc/udev/rules.d/*c922*.rules
-
-SOURCE="${BASH_SOURCE%/*}"/etc/udev/rules.d/90-webcam.$HOST.rules
-[[ -f $SOURCE ]] && sudo cp "$SOURCE" /etc/udev/rules.d/90-webcam.rules
-
-# jan 25th
-
-# # jack
-#
-# set +e
-# yes | sudo pacman -S pipewire-jack
-# set -e
-#
-# # noise-suppression-for-voice
-#
-# [[ $HOST == 'worker' ]] && {
-#   sudo pacman -S --noconfirm noise-suppression-for-voice
-#   sudo cp "${BASH_SOURCE%/*}"/home/"$USER"/.config/pipewire/pipewire.conf.d/99-rnnoise.conf "$XDG_CONFIG_HOME"/pipewire/pipewire.conf.d
-# }
+[[ $HOST == 'worker' ]] && {
+  sudo pacman -S --noconfirm noise-suppression-for-voice
+  sudo cp "${BASH_SOURCE%/*}"/home/"$USER"/.config/pipewire/pipewire.conf.d/99-rnnoise.conf "$XDG_CONFIG_HOME"/pipewire/pipewire.conf.d
+}
 
 # cleanup
 
