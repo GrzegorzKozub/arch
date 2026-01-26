bcc0460086bb0a34cc0a2c23c29f9a9bd4e508e6
greg <grzegorz.kozub@gmail.com>
2026-01-26 13:05:10 +0100

    claude code sandbox bug

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
+# claude code sandbox bug: https://github.com/anthropics/claude-code/issues/17087
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
 
 # jack
