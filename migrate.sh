854a3b67a2cb7b42779b263c6e0db2af5cc02706
greg <grzegorz.kozub@gmail.com>
2026-01-27 07:54:20 +0100

    tested noise cancelling

--- migrate.sh
+++ migrate.sh
@@ -14,7 +14,7 @@ set -e
 
 [[ $HOST == 'worker' ]] && {
   sudo pacman -S --noconfirm noise-suppression-for-voice
-  sudo cp "${BASH_SOURCE%/*}"/home/"$USER"/.config/pipewire/pipewire.conf.d/99-rnnoise.conf "$XDG_CONFIG_HOME"/pipewire/pipewire.conf.d
+  cp "${BASH_SOURCE%/*}"/home/"$USER"/.config/pipewire/pipewire.conf.d/99-rnnoise.conf "$XDG_CONFIG_HOME"/pipewire/pipewire.conf.d
 }
 
 # cleanup
