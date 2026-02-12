98b1953c3fe633c51f2875e2f2094c234d6a5403
greg <grzegorz.kozub@gmail.com>
2026-02-12 10:24:24 +0100

    add some models to ai.sh

--- ai.sh
+++ ai.sh
@@ -4,10 +4,16 @@ set -eo pipefail -ux
 # https://github.com/ggml-org/llama.cpp/discussions/14758
 # https://github.com/ggml-org/llama.cpp/discussions/15396
 
-llama-server -hf ggml-org/gpt-oss-20b-GGUF \
-  --ctx-size 131072 \
-  --n-cpu-moe 16 \
-  --no-kv-offload \
+llama-server \
+  --ctx-size 32768 \
   --flash-attn on \
   --gpu-layers all \
-  --cache-type-k q4_0 --cache-type-v q4_0
+  --n-cpu-moe 0 \
+  --no-kv-offload \
+  -hf Qwen/Qwen3-8B-GGUF:Q4_K_M
+
+  # Qwen/Qwen3-8B-GGUF:Q4_K_M
+  # ggml-org/gpt-oss-20b-GGUF
+  # unsloth/Devstral-Small-2-24B-Instruct-2512-GGUF:Q4_K_M
+  # unsloth/GLM-4.6V-Flash-GGUF:Q4_K_M
+  # unsloth/GLM-4.7-Flash-GGUF:Q4_K_M
