61d0f2673bbadd84f138527acd73ce93ebae5145
greg <grzegorz.kozub@gmail.com>
2026-03-10 08:34:57 +0100

    reset migrate

--- migrate.sh
+++ migrate.sh
@@ -5,27 +5,6 @@ set -eo pipefail -ux
 # shutdown bug: https://bbs.archlinux.org/viewtopic.php?pid=2278862
 # teams bug: https://github.com/IsmaelMartinez/teams-for-linux/issues/2169
 
-# copilot
-
-rm -rf ~/.copilot
-
-# duckdb
-
-sudo pacman -S --noconfirm duckdb
-
-# papirus
-
-[[ $(pacman -Q papirus-icon-theme-git) ]] && paru -Rs --noconfirm papirus-icon-theme-git
-[[ $(pacman -Q papirus-icon-theme) ]] || sudo pacman -S --noconfirm papirus-icon-theme
-
-# tidal
-
-uv tool uninstall tidal-dl-ng || true
-uv tool install tidal-dl-ng-for-dj
-
-rm -rf ~/.config/tidal_dl_ng
-~/code/dot/links.sh
-
 # cleanup
 
 "${BASH_SOURCE%/*}"/packages.sh
