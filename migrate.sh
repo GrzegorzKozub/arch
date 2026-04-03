1731250e1b7dd17ce9aa9a53292409c88998550a
greg <grzegorz.kozub@gmail.com>
2026-04-03 08:23:25 +0200

    migrate simplification

--- migrate.sh
+++ migrate.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
-# aws
-
-if [[ $HOST == 'worker' ]]; then
-
-  rm -rf ~/.config/aws
-  ~/code/keys/aws.sh
-
-fi
-
-# claude
-
-"${BASH_SOURCE%/*}"/data.sh
-
 # gdm
 
 sudo rm -f /var/lib/gdm/seat0/config/monitors.xml
