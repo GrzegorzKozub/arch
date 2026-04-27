f5f30217011258c393406570d1c800edbf4a01d1
greg <grzegorz.kozub@gmail.com>
2026-04-27 08:52:19 +0200

    reset

--- migrate.sh
+++ migrate.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
-# claude
-
-rm -rf ~/code/dot/claude/claude/settings.worker.json
-
-# nvim
-
-rm -rf "$XDG_DATA_HOME"/applications/nvim.desktop
-
 # cleanup
 
 "${BASH_SOURCE%/*}"/packages.sh
