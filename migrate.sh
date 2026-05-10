78065647afa2cefa4f7e5f82a41544748da0ab7f
greg <grzegorz.kozub@gmail.com>
2026-05-10 07:42:52 +0200

    tiddl

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
+# tiddl
+
+pushd ~/code/dot
+./reset.sh python
+./links.sh
+popd
+
+rm -rf ~/.config/tidal_dl_ng-dev
+
+sed -i 's/facebookUid: int$/facebookUid: Optional[int] = None/' \
+  "$XDG_DATA_HOME"/uv/tools/tiddl/lib/python3.14/site-packages/tiddl/core/auth/models.py
+
 # cleanup
 
 "${BASH_SOURCE%/*}"/packages.sh
