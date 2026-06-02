b22774a38a18abca906a1b7fcebcbc4d1fffd40d
greg <grzegorz.kozub@gmail.com>
2026-06-02 08:37:47 +0200

    try subagent statusline

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
+# claude
+
+pushd ~/code/dot
+ln -sf "$(dirname "$(realpath "$0")")"/claude/claude/subagent-statusline.sh \
+  "$XDG_CONFIG_HOME"/claude/subagent-statusline.sh
+popd
+
 # cleanup
 
 "${BASH_SOURCE%/*}"/packages.sh
