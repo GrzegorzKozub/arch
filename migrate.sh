0e553fd1d185d635cd9003513415a52f74b96d11
greg <grzegorz.kozub@gmail.com>
2026-07-10 19:18:43 +0200

    vscode permissions fix

--- migrate.sh
+++ migrate.sh
@@ -9,6 +9,13 @@ if [[ $HOST =~ ^(player|worker)$ ]]; then
   "${BASH_SOURCE%/*}"/pkg/llama-cpp-vulkan-bin.sh
 fi
 
+# vscode
+
+DIR=/usr/share/code/resources/app/extensions/copilot/node_modules/@github/copilot/sdk/prebuilds/linux-x64
+if [[ -d $DIR ]]; then
+  sudo chmod 775 "$DIR"
+fi
+
 # workaround https://bbs.archlinux.org/viewtopic.php?id=307879
 
 if [[ $(systemctl is-enabled NetworkManager-initrd.service 2> /dev/null) != masked ]]; then
