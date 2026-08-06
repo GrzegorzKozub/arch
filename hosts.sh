c3faf76cb0e0b0a2984a01187238ca5b12d5268d
greg <grzegorz.kozub@gmail.com>
2026-08-06 12:45:26 +0200

    final batch of audience proxy migration

--- hosts.sh
+++ hosts.sh
@@ -2,12 +2,7 @@
 set -eo pipefail -ux
 
 LIST=(
-  8favxfpaq6.execute-api.eu-west-1.amazonaws.com
-  ANUNLB-LB-VC5YGOQNTGG4-7707d28b7ca45024.elb.eu-west-1.amazonaws.com
-  WAWNLB2-LB-2KEW9GHX6ZHK-3cd7f3576423cce8.elb.eu-west-1.amazonaws.com
-  WAWNLB2-LB-BFAMMO58AJC7-ac1f28233c0b6fc5.elb.eu-west-1.amazonaws.com
   dev.apsis
-  vpce-0d42f60518b6200ca-t05wvrwd.vpce-svc-0e2cc8023f4c8b06f.eu-west-1.vpce.amazonaws.com
 )
 
 for ITEM in "${LIST[@]}"; do
