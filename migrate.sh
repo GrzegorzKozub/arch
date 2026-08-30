fea6846615141040320d924ecc78870afae80897
greg <grzegorz.kozub@gmail.com>
2026-08-30 22:27:55 +0200

    migrate zinit to zi

--- migrate.sh
+++ migrate.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 set -eo pipefail -ux
 
-# mise: claude, go
+# fonts
+
+[[ $HOST == 'player' ]] &&
+  sudo pacman -S --noconfirm noto-fonts-emoji &&
+  fc-cache -f
+
+# fsmonitor
+
+if [[ $HOST =~ ^(player|worker)$ ]]; then
+  for DIR in ~/code/*/ ~/code/*/*/; do
+    [[ -d $DIR/.git ]] || continue
+    rm -rf "$DIR"/.git/fsmonitor--daemon "$DIR"/.git/fsmonitor--daemon.ipc
+  done
+fi
+
+# llama-cpp
+
+[[ $HOST =~ ^(player|worker)$ ]] &&
+  sudo pacman -Rs --noconfirm llama-cpp-vulkan-bin || true
+
+# tidal
+
+pushd ~/code/dot
+git update-index --no-assume-unchanged tidal-hifi/tidal-hifi/config.json
+git pull
+popd
+
+# mise: claude & go
 
 rm -rf "$XDG_CACHE_HOME"/{go,goimports,gopls}
 rm -rf "$XDG_CONFIG_HOME"/go
 go clean -modcache && rm -rf "$XDG_DATA_HOME"/go
 
-sudo pacman -Rs --noconfirm go
+sudo pacman -Rs --noconfirm go || true
 
 pushd ~/code/dot && git pull && popd
 
@@ -29,41 +56,18 @@ if [[ $HOST == 'worker' ]]; then
 
 fi
 
-# fonts
-
-[[ $HOST == 'player' ]] &&
-  sudo pacman -S --noconfirm noto-fonts-emoji &&
-  fc-cache -f
-
-# fsmonitor
-
-if [[ $HOST =~ ^(player|worker)$ ]]; then
-
-  for DIR in ~/code/*/ ~/code/*/*/; do
-    [[ -d $DIR/.git ]] || continue
-    rm -rf "$DIR"/.git/fsmonitor--daemon "$DIR"/.git/fsmonitor--daemon.ipc
-  done
-
-fi
-
-# llama-cpp
-
-[[ $HOST =~ ^(player|worker)$ ]] &&
-  sudo pacman -Rs --noconfirm llama-cpp-vulkan-bin || true
-
-# tidal
-
-pushd ~/code/dot
-git update-index --no-assume-unchanged tidal-hifi/tidal-hifi/config.json
-git pull
-popd
-
-# zinit
+# zi & f-sy-h
 
 pushd ~/code/dot && git pull && ./links.sh && popd
-rm -rf ~/.cache/fsh/
-rm -rf ~/.config/fsh/
-zinit delete --yes zdharma-continuum/fast-syntax-highlighting
+
+rm -rf "$XDG_CACHE_HOME"/{f-sy-h,fsh,p10k*,zsh,zi,zinit}
+rm -rf "$XDG_CONFIG_HOME"/{fsh,zi}
+rm -rf "$XDG_DATA_HOME"/{zi,zinit}
+
+mkdir -p "$XDG_CACHE_HOME"/zsh
+mkdir -p "$XDG_DATA_HOME"/zi
+
+git clone https://github.com/z-shell/zi.git "$XDG_DATA_HOME"/zi/bin
 
 # cleanup
 
