250b094291fe52c9c2b5a502fe22e4510ab72dc9
greg <grzegorz.kozub@gmail.com>
2026-09-18 11:00:15 +0200

    cleanup work script

--- work.sh
+++ work.sh
@@ -10,12 +10,6 @@ set -eo pipefail -ux
 if [[ $HOST == 'worker' ]]; then
 
   "${BASH_SOURCE%/*}"/aws.sh
-
-  # "${BASH_SOURCE%/*}"/ansible.sh
-
-  # "${BASH_SOURCE%/*}"/dotnet.sh
-  # "${BASH_SOURCE%/*}"/java.sh
-
   "${BASH_SOURCE%/*}"/claude.sh
 
 fi
