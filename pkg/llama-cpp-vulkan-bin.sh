754c1c9c5e96e568c3087eeee52055b253b3ef2a
greg <grzegorz.kozub@gmail.com>
2026-09-24 08:53:38 +0200

    nise tiny model

--- pkg/llama-cpp-vulkan-bin.sh
+++ pkg/llama-cpp-vulkan-bin.sh
@@ -5,7 +5,7 @@ pushd "${BASH_SOURCE%/*}"/llama-cpp-vulkan-bin
 
 # shellcheck disable=SC1091
 AVAILABLE=$(source PKGBUILD && pkgver)
-INSTALLED=$(pacman -Q llama-cpp-vulkan-bin 2> /dev/null | awk '{print $2}' | cut -d- -f1)
+INSTALLED=$(pacman -Q llama-cpp-vulkan-bin 2> /dev/null | awk '{print $2}' | cut -d- -f1) || true
 
 if [[ $AVAILABLE != "$INSTALLED" ]]; then
 
